11fa9863bf3454748b6046b7749b26f8748a4409
jjz <woaf1003@gmail.com>
2016-09-12 22:07:10 +0800

    add block

--- Block.go
+++ Block.go
@@ -1,11 +1,51 @@
 package gobtcparse
 
+import (
+	"encoding/binary"
+	"bytes"
+	"encoding/hex"
+)
 
+var EmptyByte = []byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
 
 type Block struct {
-	Raw [] byte
-	Hash string
-	Height uint
+	Raw            [] byte
+	Hash           string
+	Height         uint
+	Transactions   []*Transaction
+	Version        uint32
+	MerkleRoot     string
+	BlockTime      uint32
+	Bits           uint32
+	Nonce          uint32
+	Size           uint32
+	TransactionCnt uint32
+	TotalBTC       uint64
+	BlockReward    float64
+	PrevBlock      string
+	NextBlock      string
+}
 
+func PasetBlock(raw [] byte) (block *Block, err error) {
 
+	block = new(Block)
+	block.Raw = raw
+	block.Hash = ToHash256String(raw[:80])
+	block.Version = binary.LittleEndian.Uint32(raw[0:4])
+
+	if !bytes.Equal(raw[4:36], EmptyByte) {
+
+		block.PrevBlock = ToHash256String(raw[4:36])
+
+	}
+	block.MerkleRoot = hex.EncodeToString(raw[36:68])
+	block.BlockTime = binary.LittleEndian.Uint32(raw[68:72])
+	block.Bits = binary.LittleEndian.Uint32(raw[72:76])
+	block.Nonce = binary.LittleEndian.Uint32(raw[76:80])
+	block.Size = uint32(len(raw))
+
+	txs, _ := ParseTranscation(raw[80:])
+	block.Transactions = txs
+	block.TransactionCnt = len(txs)
+	return
 }
