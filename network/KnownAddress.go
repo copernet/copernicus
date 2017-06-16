25cf7a5d2b78890b688906054131141f407ab926
jjz <woaf1003@gmail.com>
2017-06-16 09:00:37 +0800

    add KnownAddress

---
+++ network/KnownAddress.go
@@ -0,0 +1,55 @@
+package network
+
+import (
+	"copernicus/msg"
+	"time"
+)
+
+type KnownAddress struct {
+	NetAddress  *msg.PeerAddress
+	SrcAddress  *msg.PeerAddress
+	attempts    int
+	LastAttempt time.Time
+	lastSuccess time.Time
+	tried       bool
+	refs        int
+}
+
+func (knownAddress *KnownAddress) Chance() float64 {
+	now := time.Now()
+	lastSeen := now.Sub(knownAddress.NetAddress.Timestamp)
+	lastAttempt := now.Sub(knownAddress.LastAttempt)
+	if lastSeen < 0 {
+		lastSeen = 0
+	}
+	if lastAttempt < 0 {
+		lastAttempt = 0
+	}
+	chance := .0
+	if lastAttempt < 10*time.Minute {
+		chance *= 0.01
+	}
+	for i := knownAddress.attempts; i > 0; i-- {
+		chance /= 1.5
+	}
+	return chance
+}
+func (knownAddress *KnownAddress) IsBad() bool {
+	if knownAddress.LastAttempt.After(time.Now().Add(-1 * time.Minute)) {
+		return false
+	}
+	if knownAddress.NetAddress.Timestamp.After(time.Now().Add(10 * time.Minute)) {
+		return true
+	}
+	if knownAddress.NetAddress.Timestamp.Before(time.Now().Add(-1 * NUM_MISSING_DAYS * time.Hour * 24)) {
+		return true
+	}
+	if knownAddress.lastSuccess.IsZero() && knownAddress.attempts >= NUMRETIES {
+		return true
+	}
+	if !knownAddress.lastSuccess.After(time.Now().Add(-1 * MIN_BAD_DAYS * time.Hour * 24)) && knownAddress.attempts >= MAX_FAILURES {
+		return true
+	}
+	return false
+
+}
--- peer/PeeManager.go
+++ peer/PeeManager.go
@@ -1,1 +1,14 @@
 package peer
+
+import "copernicus/protocol"
+
+type PeerManager struct {
+	bytesReceived uint64
+	bytesSend     uint64
+	started       int32
+	shutdown      int32
+	shutdownSched int32
+
+	chainParams *protocol.BitcoinParams
+
+}
