d126f1c6e5d6308ee8469f88759bd89e4ad771cf
jjz <woaf1003@gmail.com>
2017-06-18 18:43:17 +0800

    savePeers

--- network/NetAddressManager.go
+++ network/NetAddressManager.go
@@ -8,11 +8,15 @@ import (
 	"copernicus/crypto"
 	"encoding/binary"
 	"github.com/siddontang/go/log"
+	"time"
+	"os"
+	"encoding/json"
 )
 
 const (
 	NEW_BUCKET_COUNT         = 1024
 	TRIED_BUCKET_COUNT       = 64
+	TRIED_BUCKETS_PEER_GROUP = 8
 	NUM_MISSING_DAYS         = 30
 	NUMRETIES                = 3
 	MIN_BAD_DAYS             = 7
@@ -20,6 +24,8 @@ const (
 	NEW_BUCKETS_PEER_ADDRESS = 8
 	NEW_BUCKETS_PEER_GROUP   = 64
 	NEW_BUCKET_SIZE          = 64
+	DUMP_ADDRESS_INTERVAL    = time.Minute * 10
+	SERIALISATION_VERSION    = 1
 )
 
 type NetAddressManager struct {
@@ -121,7 +127,104 @@ func (addressManager *NetAddressManager) expireNew(bucket int) {
 	}
 
 }
-func (addressManager *NetAddressManager)pickTried()
+func (addressManager *NetAddressManager) pickTried(bucket int) *list.Element {
+	var oldest *KnownAddress
+	var oldestElem *list.Element
+	for e := addressManager.addressTried[bucket].Front(); e != nil; e.Next() {
+		knownAddress := e.Value.(*KnownAddress)
+		if oldest == nil || oldest.NetAddress.Timestamp.After(knownAddress.NetAddress.Timestamp) {
+			oldestElem = e
+			oldest = knownAddress
+		}
+	}
+	return oldestElem
+}
+func (addressManager *NetAddressManager) getTriedBucket(netAddress *PeerAddress) int {
+	dataFrist := []byte{}
+	dataFrist = append(dataFrist, addressManager.key[:]...)
+	dataFrist = append(dataFrist, []byte(netAddress.NetAddressKey())...)
+	hashFrist := crypto.DoubleSha256Bytes(dataFrist)
+	hash64 := binary.LittleEndian.Uint64(hashFrist)
+	hash64 %= TRIED_BUCKETS_PEER_GROUP
+	var hashBuf [8]byte
+	binary.LittleEndian.PutUint64(hashBuf[:], hash64)
+	dataSecond := []byte{}
+	dataSecond = append(dataSecond, addressManager.key[:]...)
+	dataSecond = append(dataSecond, netAddress.GroupKey()...)
+	dataSecond = append(dataSecond, hashBuf[:]...)
+	hashSecond := crypto.DoubleSha256Bytes(dataSecond)
+	return int(binary.LittleEndian.Uint64(hashSecond) % TRIED_BUCKET_COUNT)
+
+}
+func (addressManager *NetAddressManager) savePeers() {
+	addressManager.lock.Lock()
+	defer addressManager.lock.Unlock()
+	serializedAddressManager := new(SerializedAddressManager)
+	serializedAddressManager.Version = SERIALISATION_VERSION
+	copy(serializedAddressManager.Key[:], addressManager.key[:])
+	serializedAddressManager.Addresses = make([]*SerializedKnownAddress, len(addressManager.addressIndex))
+	i := 0
+	for k, v := range addressManager.addressIndex {
+		serializedKnownAddress := new(SerializedKnownAddress)
+		serializedKnownAddress.AddressString = k
+		serializedKnownAddress.TimeStamp = v.NetAddress.Timestamp.Unix()
+		serializedKnownAddress.Source = v.SrcAddress.NetAddressKey()
+		serializedKnownAddress.LastAttempt = v.LastAttempt.Unix()
+		serializedKnownAddress.LastSuccess = v.lastSuccess.Unix()
+		serializedAddressManager.Addresses[i] = serializedKnownAddress
+		i++
+	}
+	for i := range addressManager.addressNew {
+		serializedAddressManager.NewBuckets[i] = make([]string, len(addressManager.addressNew[i]))
+		j := 0
+		for k := range addressManager.addressNew[i] {
+			serializedAddressManager.NewBuckets[i][j] = k
+			j++
+		}
+	}
+	for i := range addressManager.addressTried {
+		serializedAddressManager.TriedBuckets[i] = make([]string, addressManager.addressTried[i].Len())
+		j := 0
+		for e := addressManager.addressTried[i].Front(); e != nil; e = e.Next() {
+			knownAddress := e.Value.(*KnownAddress)
+			serializedAddressManager.TriedBuckets[i][j] = knownAddress.NetAddress.NetAddressKey()
+			j++
+		}
+	}
+	w, err := os.Create(addressManager.peersFile)
+	if err != nil {
+		log.Errorf("Error opening file %s :%v", addressManager.peersFile, err)
+		return
+	}
+	newEncoder := json.NewEncoder(w)
+	defer w.Close()
+
+	if err := newEncoder.Encode(&serializedAddressManager); err != nil {
+		log.Errorf("Failed to encode file %s :%v", addressManager.peersFile, err)
+	}
+}
+func (addressManager *NetAddressManager) addressHandler() {
+	dumpAddressTicker := time.NewTicker(DUMP_ADDRESS_INTERVAL)
+	defer dumpAddressTicker.Stop()
+out:
+	for {
+		select {
+		case <-dumpAddressTicker.C:
+			addressManager.savePeers()
+
+		case <-addressManager.quit:
+			break out
+		}
+	}
+	addressManager.savePeers()
+	addressManager.waitGroup.Done()
+	log.Trace("address handler done ")
+}
+func(addressManager *NetAddressManager)loadPeers(){
+	addressManager.lock.Lock()
+	defer addressManager.lock.Unlock()
+
+}
 func (addressManger *NetAddressManager) getNewBucket(netAddr, srcAddr *PeerAddress) int {
 	// bitcoind:
 	// doublesha256(key + sourcegroup + int64(doublesha256(key + group + sourcegroup))%bucket_per_source_group) % num_new_buckets
@@ -142,6 +245,7 @@ func (addressManger *NetAddressManager) getNewBucket(netAddr, srcAddr *PeerAddre
 	return int(binary.LittleEndian.Uint64(hashSecond) % NEW_BUCKET_COUNT)
 
 }
+
 func (a *NetAddressManager) find(peerAddress *PeerAddress) *KnownAddress {
 	return a.addressIndex[peerAddress.NetAddressKey()]
 }
