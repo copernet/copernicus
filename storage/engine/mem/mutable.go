43c305a25c4fee68b186c3aa4558c29cdd093bc1
jjz <woaf1003@gmail.com>
2017-06-28 11:28:15 +0800

    remove ssh_known_hosts

--- storage/engine/mem/mutable.go
+++ storage/engine/mem/mutable.go
@@ -2,7 +2,6 @@ package mem
 
 import (
 	"bytes"
-	"math/rand"
 )
 
 type Mutable struct {
@@ -68,102 +67,102 @@ func (m *Mutable) relinkGrandparent(node, parent, grandparent *Node) {
 	}
 }
 
-func (m *Mutable) Put(key, value []byte) {
-	if value == nil {
-		value = emptySlice
-	}
-	if m.root == nil {
-		node := NewNode(key, value, rand.Int())
-		m.count = 1
-		m.totalSize = node.Size()
-		m.root = node
-		return
-	}
-	var parents parentStack
-	var compareResult int
-	for node := m.root; node != nil; {
-		parents.Push(node)
-		compareResult = bytes.Compare(key, node.key)
-		if compareResult < 0 {
-			node = node.left
-			continue
-		}
-		if compareResult > 0 {
-			node = node.right
-			continue
-		}
-		m.totalSize -= uint64(len(node.value))
-		m.totalSize += uint64(len(value))
-		node.value = value
-		return
-	}
-	node := NewNode(key, value, rand.Int())
-	m.count++
-	m.totalSize += node.Size()
-	parent := parents.At(0)
-	if compareResult < 0 {
-		parent.left = node
-	} else {
-		parent.right = node
-	}
-	for parents.Len() > 0 {
-		parent = parents.Pop()
-		if node.priority >= parent.priority {
-			break
-		}
-		if parent.left == node {
-			node.right, parent.left = parent, node.right
-		} else {
-			node.left, parent.right = parent, node.left
-		}
-		m.relinkGrandparent(node, parent, parents.At(0))
-	}
-}
+//func (m *Mutable) Put(key, value []byte) {
+//	if value == nil {
+//		value = emptySlice
+//	}
+//	if m.root == nil {
+//		node := NewNode(key, value, rand.Int())
+//		m.count = 1
+//		m.totalSize = node.Size()
+//		m.root = node
+//		return
+//	}
+//	var parents parentStack
+//	var compareResult int
+//	for node := m.root; node != nil; {
+//		parents.Push(node)
+//		compareResult = bytes.Compare(key, node.key)
+//		if compareResult < 0 {
+//			node = node.left
+//			continue
+//		}
+//		if compareResult > 0 {
+//			node = node.right
+//			continue
+//		}
+//		m.totalSize -= uint64(len(node.value))
+//		m.totalSize += uint64(len(value))
+//		node.value = value
+//		return
+//	}
+//	node := NewNode(key, value, rand.Int())
+//	m.count++
+//	m.totalSize += node.Size()
+//	parent := parents.At(0)
+//	if compareResult < 0 {
+//		parent.left = node
+//	} else {
+//		parent.right = node
+//	}
+//	for parents.Len() > 0 {
+//		parent = parents.Pop()
+//		if node.priority >= parent.priority {
+//			break
+//		}
+//		if parent.left == node {
+//			node.right, parent.left = parent, node.right
+//		} else {
+//			node.left, parent.right = parent, node.left
+//		}
+//		m.relinkGrandparent(node, parent, parents.At(0))
+//	}
+//}
 
-func (m *Mutable) Delete(key []byte) {
-	node, parent := m.get(key)
-	if node == nil {
-		return
-	}
-	if parent == nil && node.left == nil && node.right == nil {
-		t.root = nil
-		t.count = 0
-		t.totalSize = 0
-		return
-	}
-	var isLeft bool
-	var child *Node
-	for node.left != nil || node.right != nil {
-		// Choose the child with the higher priority.
-		if node.left == nil {
-			child = node.right
-			isLeft = false
-		} else if node.right == nil {
-			child = node.left
-			isLeft = true
-		} else if node.left.priority >= node.right.priority {
-			child = node.left
-			isLeft = true
-		} else {
-			child = node.right
-			isLeft = false
-		}
-		if isLeft {
-			child.right, node.left = node, child.right
-		} else {
-			child.left, node.right = node, child.left
-		}
-		m.relinkGrandparent(child, node, parent)
-		parent = child
-	}
-	if parent.right == node {
-		parent.right = nil
-	} else {
-		parent.left = nil
-	}
-	m.count--
-	m.totalSize -= node.Size()
-}
+//func (m *Mutable) Delete(key []byte) {
+//	node, parent := m.get(key)
+//	if node == nil {
+//		return
+//	}
+//	if parent == nil && node.left == nil && node.right == nil {
+//		t.root = nil
+//		t.count = 0
+//		t.totalSize = 0
+//		return
+//	}
+//	var isLeft bool
+//	var child *Node
+//	for node.left != nil || node.right != nil {
+//		// Choose the child with the higher priority.
+//		if node.left == nil {
+//			child = node.right
+//			isLeft = false
+//		} else if node.right == nil {
+//			child = node.left
+//			isLeft = true
+//		} else if node.left.priority >= node.right.priority {
+//			child = node.left
+//			isLeft = true
+//		} else {
+//			child = node.right
+//			isLeft = false
+//		}
+//		if isLeft {
+//			child.right, node.left = node, child.right
+//		} else {
+//			child.left, node.right = node, child.left
+//		}
+//		m.relinkGrandparent(child, node, parent)
+//		parent = child
+//	}
+//	if parent.right == node {
+//		parent.right = nil
+//	} else {
+//		parent.left = nil
+//	}
+//	m.count--
+//	m.totalSize -= node.Size()
+//}
 
 func (m *Mutable) ForEach(fn func(k, v []byte) bool) {
 	var parents Stack
--- storage/engine/mem/stack.go
+++ storage/engine/mem/stack.go
@@ -9,85 +9,85 @@ var (
 	emptySlice = make([]byte, 0)
 )
 
-type Node struct {
-	key      []byte
-	value    []byte
-	priority int
-	left     *node
-	right    *node
-}
+//type Node struct {
+//	key      []byte
+//	value    []byte
+//	priority int
+//	left     *node
+//	right    *node
+//}
 
-func NewNode(key, value []byte, priority int) *Node {
-	node := &Node{
-		key:      key,
-		value:    value,
-		priority: priority,
-	}
-	return node
-}
-func (m *Node) Size() uint64 {
-	return NODE_FIELDS_SIZE + uint64(len(m.key)+len(node.value))
-}
-
-func (m *Node) CLone() *Node {
-	node := &Node{
-		key:      m.key,
-		value:    m.value,
-		priority: m.priority,
-		left:     m.left,
-		right:    m.right,
-	}
-	return node
-}
-
-type Stack struct {
-	index    int
-	items    [STATIC_DEPTH]*Node
-	overflow []*Node
-}
-
-func (m *Stack) Len() int {
-	return m.index
-}
-
-func (m *Stack) At(n int) *Node {
-	index := s.index - n - 1
-	if index < 0 {
-		return nil
-	}
-	if index < STATIC_DEPTH {
-		return s.items[index]
-	}
-	return s.overflow[index-STATIC_DEPTH]
-}
-
-func (m *Stack) Pop() *Node {
-	if s.index == 0 {
-		return nil
-	}
-	s.index--
-	if s.index < STATIC_DEPTH {
-		node = s.items[s.index]
-		s.items[s.index] = nil
-		return node
-	}
-	node := s.overflow[s.index-STATIC_DEPTH]
-	s.overflow[s.index-STATIC_DEPTH] = nil
-	return node
-}
-
-func (m *Stack) Push(node *Node) {
-	if m.index < STATIC_DEPTH {
-		m.items[s.index] = node
-		m.index++
-		return
-	}
-	index := m.index - STATIC_DEPTH
-	if index+1 > cap(m.overflow) {
-		overflow := make([]*Node, index+1)
-		copy(overflow, m.overflow)
-		m.overflow = overflow
-	}
-	m.overflow[index] = node
-	m.index++
-}
+//func NewNode(key, value []byte, priority int) *Node {
+//	node := &Node{
+//		key:      key,
+//		value:    value,
+//		priority: priority,
+//	}
+//	return node
+//}
+//func (m *Node) Size() uint64 {
+//	return NODE_FIELDS_SIZE + uint64(len(m.key)+len(node.value))
+//}
+//
+//func (m *Node) CLone() *Node {
+//	node := &Node{
+//		key:      m.key,
+//		value:    m.value,
+//		priority: m.priority,
+//		left:     m.left,
+//		right:    m.right,
+//	}
+//	return node
+//}
+//
+//type Stack struct {
+//	index    int
+//	items    [STATIC_DEPTH]*Node
+//	overflow []*Node
+//}
+//
+//func (m *Stack) Len() int {
+//	return m.index
+//}
+//
+//func (m *Stack) At(n int) *Node {
+//	index := s.index - n - 1
+//	if index < 0 {
+//		return nil
+//	}
+//	if index < STATIC_DEPTH {
+//		return s.items[index]
+//	}
+//	return s.overflow[index-STATIC_DEPTH]
+//}
+//
+//func (m *Stack) Pop() *Node {
+//	if s.index == 0 {
+//		return nil
+//	}
+//	s.index--
+//	if s.index < STATIC_DEPTH {
+//		node = s.items[s.index]
+//		s.items[s.index] = nil
+//		return node
+//	}
+//	node := s.overflow[s.index-STATIC_DEPTH]
+//	s.overflow[s.index-STATIC_DEPTH] = nil
+//	return node
+//}
+//
+//func (m *Stack) Push(node *Node) {
+//	if m.index < STATIC_DEPTH {
+//		m.items[s.index] = node
+//		m.index++
+//		return
+//	}
+//	index := m.index - STATIC_DEPTH
+//	if index+1 > cap(m.overflow) {
+//		overflow := make([]*Node, index+1)
+//		copy(overflow, m.overflow)
+//		m.overflow = overflow
+//	}
+//	m.overflow[index] = node
+//	m.index++
+//}
