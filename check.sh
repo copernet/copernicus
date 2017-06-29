aaad3c20c9d8a796fd81a012a77552e824943441
jjz <woaf1003@gmail.com>
2017-06-29 20:17:19 +0800

    check test

--- check.sh
+++ check.sh
@@ -12,23 +12,24 @@ fi
 
 linter_targets=$(glide novendor)
 
-#test -z "$(gometalinter -j 4 --disable-all \
-#--enable=gofmt \
-#--enable=golint \
-#--enable=vet \
-#--enable=gosimple \
-#--enable=unconvert \
-#--deadline=10m $linter_targets 2>&1 | grep -v 'ALL_CAPS\|OP_' 2>&1 | tee /dev/stderr)"
-
-
-
-gometalinter -j 4 --disable-all \
+test -z "$(gometalinter -j 4 --disable-all \
 --enable=gofmt \
 --enable=golint \
 --enable=vet \
 --enable=gosimple \
 --enable=unconvert \
 --exclude='should have comment' \
---deadline=10m $linter_targets 2>&1 | grep -v 'ALL_CAPS\|OP_' 2>&1 | tee /dev/stderr
+--deadline=10m $linter_targets 2>&1 | grep -v 'ALL_CAPS\|OP_' 2>&1 | tee /dev/stderr)"
+
+
+
+#gometalinter -j 4 --disable-all \
+#--enable=gofmt \
+#--enable=golint \
+#--enable=vet \
+#--enable=gosimple \
+#--enable=unconvert \
+#--exclude='should have comment' \
+#--deadline=10m $linter_targets 2>&1 | grep -v 'ALL_CAPS\|OP_' 2>&1 | tee /dev/stderr
 
 env GORACE="halt_on_error=1" go test -race -tags rpctest $linter_targets
