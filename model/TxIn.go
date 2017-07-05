9b5077f9b17aaf022659dfed0a8f35e639b01b7c
jjz <woaf1003@gmail.com>
2017-07-05 14:58:06 +0800

    add readTxIn and WriteTxIn

--- model/TxIn.go
+++ model/TxIn.go
@@ -1,11 +1,16 @@
 package model
 
-import "github.com/btccom/copernicus/utils"
+import (
+	"encoding/binary"
+	"github.com/btccom/copernicus/protocol"
+	"github.com/btccom/copernicus/utils"
+	"io"
+)
 
 type TxIn struct {
 	PreviousOutPoint *OutPoint
 	ScriptSig        []byte
-	Sequence         uint32
+	Sequence         uint32 //todo ?
 }
 
 func (txIn *TxIn) SerializeSize() int {
@@ -19,4 +24,29 @@ func (txIn *TxIn) SerializeSize() int {
 func NewTxIn(prevOut *OutPoint, pkScript []byte) *TxIn {
 	txIn := TxIn{PreviousOutPoint: prevOut, ScriptSig: pkScript, Sequence: MaxTxInSequenceNum}
 	return &txIn
+}
+
+func (txIn *TxIn) ReadTxIn(reader io.Reader, pver uint32, version int32) error {
+	err := txIn.PreviousOutPoint.ReadOutPoint(reader, pver, version)
+	if err != nil {
+		return err
+	}
+	txIn.ScriptSig, err = ReadScript(reader, pver, MaxmessagePayload, "tx input signature script")
+	if err != nil {
+		return err
+	}
+	return protocol.ReadElement(reader, &txIn.Sequence)
+
+}
+func (txIn *TxIn) WriteTxIn(writer io.Writer, pver uint32, version int32) error {
+	err := txIn.PreviousOutPoint.WriteOutPoint(writer, pver, version)
+	if err != nil {
+		return err
+	}
+	err = utils.WriteVarBytes(writer, pver, txIn.ScriptSig)
+	if err != nil {
+		return err
+	}
+
+	return utils.BinarySerializer.PutUint32(writer, binary.LittleEndian, txIn.Sequence)
 }
