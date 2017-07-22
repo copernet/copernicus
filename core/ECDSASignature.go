77394243b6809eb399b160817fd132b81d57724e
jjz <woaf1003@gmail.com>
2017-07-22 17:30:16 +0800

    add IsValidSignatureEncoding

--- core/ECDSASignature.go
+++ core/ECDSASignature.go
@@ -43,3 +43,75 @@ func Sign(privKey *PrivateKey, hash []byte) ([]byte, error) {
 	_, ret, _ := secp256k1.EcdsaSignatureSerializeDer(secp256k1Context, signature)
 	return ret, nil
 }
+
+/**
+ * IsValidSignatureEncoding  A canonical signature exists of: <30> <total len> <02> <len R> <R> <02> <len S> <S> <hashtype>
+ * Where R and S are not negative (their first byte has its highest bit not set), and not
+ * excessively padded (do not start with a 0 byte, unless an otherwise negative number follows,
+ * in which case a single 0 byte is necessary and even required).
+ *
+ * See https://bitcointalk.org/index.php?topic=8392.msg127623#msg127623
+ *
+ * This function is consensus-critical since BIP66.
+ */
+
+func IsValidSignatureEncoding(signs []byte) bool {
+	// Format: 0x30 [total-length] 0x02 [R-length] [R] 0x02 [S-length] [S] [sighash]
+	// * total-length: 1-byte length descriptor of everything that follows,
+	//   excluding the sighash byte.
+	// * R-length: 1-byte length descriptor of the R value that follows.
+	// * R: arbitrary-length big-endian encoded R value. It must use the shortest
+	//   possible encoding for a positive integers (which means no null bytes at
+	//   the start, except a single one when the next byte has its highest bit set).
+	// * S-length: 1-byte length descriptor of the S value that follows.
+	// * S: arbitrary-length big-endian encoded S value. The same rules apply.
+	// * sighash: 1-byte value indicating what data is hashed (not part of the DER
+	//   signature)
+	signsLen := len(signs)
+	if signsLen < 9 {
+		return false
+	}
+	if signsLen > 73 {
+		return false
+	}
+	if signs[0] != 0x30 {
+		return false
+	}
+	if int(signs[1]) != (signsLen - 3) {
+		return false
+	}
+	lenR := signs[3]
+	if int(5+lenR) >= signsLen {
+		return false
+	}
+	lenS := signs[5+lenR]
+	if int(lenR+lenS+7) != signsLen {
+		return false
+	}
+	if signs[2] != 0x02 {
+		return false
+	}
+	if lenR == 0 {
+		return false
+	}
+	if (signs[4] & 0x80) == 1 {
+		return false
+	}
+	if lenR > 1 && (signs[4] == 0x00) && (signs[5]&0x80) != 1 {
+		return false
+	}
+	if signs[lenR+4] != 0x02 {
+		return false
+	}
+	if lenS == 0 {
+		return false
+	}
+	if signs[lenR+6]&0x80 == 1 {
+		return false
+	}
+	if lenS > 1 && (signs[lenR+6] == 0x00) && (signs[lenR+7]&0x80) != 1 {
+		return false
+	}
+	return true
+
+}
