85474e0e0fc6c2e923112d121c730de2ec6a0c50
jjz <woaf1003@gmail.com>
2017-08-06 08:28:41 +0800

    implement stack

--- scripts/Stack.go
+++ scripts/Stack.go
@@ -1,17 +1,28 @@
 package scripts
 
 type Stack struct {
-	vector [][]byte
+	stk [][]byte
 }
 
-func (stack *Stack) Push() bool {
-	return false
+func (s *Stack) PushBytes(bytes []byte) bool {
+	s.stk = append(s.stk, bytes)
+	return true
 }
 
-func (stack *Stack) Pop() bool {
-	return false
+// PopBytes return valuas:
+// first []byte is the value which is popped, second []byte is the error information if failed
+func (s *Stack) PopBytes() ([]byte, []byte) {
+	sz := int32(len(s.stk))
+	if sz <= 0 {
+		return nil, []byte("the stack is empty")
+	}
+	so := s.stk[sz-1]
+	s.stk = s.stk[:sz-1]
+
+	return so, nil
 }
 
 func NewStack() *Stack {
-	return nil
+	stack := Stack{}
+	return &stack
 }
