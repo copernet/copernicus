30ceac0484b3e2544abb64deab5dbdf6a5f0ea85
jjz <woaf1003@gmail.com>
2017-09-12 17:19:53 +0800

    add primitives

--- mempool/TxMempoolEntry.go
+++ mempool/TxMempoolEntry.go
@@ -15,4 +15,16 @@ type TxMempoolEntry struct {
 	SpendsCoinbase    bool
 	SigOpCount        int64
 	FeeDelta          int64
+
+	LockPoints *LockPoints
+	// Information about descendants of this transaction that are in the
+	// mempool; if we remove this transaction we must remove all of these
+	// descendants as well.  if nCountWithDescendants is 0, treat this entry as
+	// dirty, and nSizeWithDescendants and nModFeesWithDescendants will not be
+	// correct.
+	//!< number of descendant transactions
+	CountWithDescendants    uint64
+	SizeWithDescendants     uint64
+	ModFeesWithDescendants  int64
+	SigOpCoungWithAncestors int64
 }
---
+++ primitives/TxCheck.go
@@ -0,0 +1,45 @@
+package primitives
+
+import (
+	"errors"
+
+	"github.com/btcboost/copernicus/mempool"
+	"github.com/btcboost/copernicus/model"
+	"github.com/btcboost/copernicus/utxo"
+)
+
+func TxCheckChain(tx *model.Tx) (int, error) {
+	TxInsLen := len(tx.Ins)
+	var PreTxsOutTotalMoney int64
+	for i := 0; i < TxInsLen; i++ {
+		PreTx := mempool.GetTxFromMemPool(tx.Ins[i].PreviousOutPoint.Hash)
+		if PreTx != nil {
+			if PreTx.ValState == model.TX_ORPHAN {
+				return model.TX_ORPHAN, errors.New("previous TX is orphan")
+			}
+			PreTxsOutTotalMoney += PreTx.Outs[tx.Ins[i].PreviousOutPoint.Index].Value
+		} else {
+			PreTx = utxo.GetTxFromUTXO(tx.Ins[i].PreviousOutPoint.Hash)
+			if PreTx == nil {
+				return model.TX_ORPHAN, errors.New("UTXO has no previous tx")
+			}
+			PreTxsOutTotalMoney += PreTx.Outs[tx.Ins[i].PreviousOutPoint.Index].Value
+		}
+	}
+	TxOutsLen := len(tx.Outs)
+	var TotalOutsMoney int64
+	for j := 0; j < TxOutsLen; j++ {
+		TotalOutsMoney += tx.Outs[j].Value
+	}
+	if PreTxsOutTotalMoney < TotalOutsMoney {
+		return model.TX_INVALID, errors.New("Ins' money < outs' money")
+	}
+
+	for k := 0; k < TxInsLen; k++ {
+		ret, error := tx.Ins[k].Script.Eval()
+		if ret != 0 {
+			return ret, error
+		}
+	}
+	return 0, nil
+}
