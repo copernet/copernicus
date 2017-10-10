3bc4ffbbbe91d271d7866e7b876cf3dee05ae27a
ludete <yyxyong@163.com>
2017-10-10 18:18:41 +0800

    add MemUsage And test

--- mempool/MemUsage.go
+++ mempool/MemUsage.go
@@ -2,10 +2,58 @@ package mempool
 
 import (
 	"github.com/btcboost/copernicus/model"
+	"gopkg.in/fatih/set.v0"
+	"unsafe"
 )
 
 // RecursiveDynamicUsage todo need to dynamically calculate the memory footprint of the object , haven't figured out
 // how to calculate it better yet , How about the size  of the object's serialization
 func RecursiveDynamicUsage(tx *model.Tx) int {
 	return tx.SerializeSize()
+}
+
+func DynamicUsage(Item interface{}) int {
+	size := 0
+	switch typeOf := Item.(type) {
+	case *set.Set:
+		num := typeOf.Size()
+		switch v := typeOf.List()[0].(type) {
+		case TxMempoolEntry:
+			size = num * int(unsafe.Sizeof(v))
+		case *TxMempoolEntry:
+			size = num * int(unsafe.Sizeof(v))
+		case int:
+			size = num * int(unsafe.Sizeof(v))
+
+		}
+	case TxMempoolEntry:
+		size = int(unsafe.Sizeof(typeOf))
+	case *TxMempoolEntry:
+		size = int(unsafe.Sizeof(typeOf))
+
+	}
+	return size
+}
+
+/**
+ * Compute the memory used for dynamically allocated but owned data structures.
+ * For generic data types, this is *not* recursive.
+ * DynamicUsage(vector<vector<int>>) will compute the memory used for the
+ * vector<int>'s, but not for the ints inside. This is for efficiency reasons,
+ * as these functions are intended to be fast. If application data structures
+ * require more accurate inner accounting, they should iterate themselves, or
+ * use more efficient caching + updating on modification.
+ */
+func MallocUsage(alloc int) int {
+	tmp := new(int)
+	// Measured on libc6 2.19 on Linux
+	if alloc == 0 {
+		return 0
+	} else if unsafe.Sizeof(tmp) == 8 {
+		return ((alloc + 31) >> 4) << 4
+	} else if unsafe.Sizeof(tmp) == 4 {
+		return ((alloc + 15) >> 3) << 3
+	} else {
+		panic("the platForm is not supported !!!")
+	}
 }
---
+++ mempool/MemUsage_test.go
@@ -0,0 +1,43 @@
+package mempool
+
+import (
+	"gopkg.in/fatih/set.v0"
+	"testing"
+)
+
+func TestDynamicUsage(t *testing.T) {
+	entry := TxMempoolEntry{}
+	size := DynamicUsage(entry)
+	if size != 168 {
+		t.Errorf("current TxMempoolEntry type Size : %d except Size : 168", size)
+	}
+	entryPtr := &TxMempoolEntry{}
+	size = DynamicUsage(entryPtr)
+	if size != 8 {
+		t.Errorf("current *TxMempoolEntry type Size : %d except Size : 8", size)
+	}
+
+	setInt := set.New()
+	setInt.Add(1)
+	setInt.Add(2)
+	setInt.Add(3)
+	size = DynamicUsage(setInt)
+	if size != 24 {
+		t.Errorf("current IntSet type Size : %d except Size : 24", size)
+	}
+
+	setEntry := set.New(TxMempoolEntry{Fee: 10})
+	setEntry.Add(TxMempoolEntry{FeeDelta: 100})
+	setEntry.Add(TxMempoolEntry{Fee: 90})
+	setEntry.Add(TxMempoolEntry{Fee: 98})
+	size = DynamicUsage(setEntry)
+	if size != 168*4 {
+		t.Errorf("current  TxMempoolEntrySet type Size : %d except Size : %d, element Number : %d",
+			size, 168*4, setEntry.Size())
+	}
+
+	size = MallocUsage(10)
+	if size != 32 {
+		t.Errorf("current Size : %d, except Size : %d ", size, 32)
+	}
+}
