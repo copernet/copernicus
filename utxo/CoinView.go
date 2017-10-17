9c73fbf8601c5ccdf9f3e1b0d064239579814e2b
jjz <woaf1003@gmail.com>
2017-10-17 09:20:53 +0800

    add cacheCoins of CoinView

--- utxo/CoinView.go
+++ utxo/CoinView.go
@@ -1,7 +1,12 @@
 package utxo
 
-import "github.com/btcboost/copernicus/utils"
+import (
+	"github.com/btcboost/copernicus/model"
+	"github.com/btcboost/copernicus/utils"
+)
 
 type CoinView struct {
-	hashBlock utils.Hash
+	hashBlock        utils.Hash
+	cacheCoins       map[model.OutPoint]*CoinsCacheEntry
+	cachedCoinsUsage int32
 }
