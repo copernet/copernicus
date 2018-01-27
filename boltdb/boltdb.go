e629411efc5367f71e7b9b8d4a71de9321e82a82
jjz <woaf1003@gmail.com>
2018-01-27 14:12:32 +0800

    open and close db

--- boltdb/boltdb.go
+++ boltdb/boltdb.go
@@ -1,35 +1,49 @@
 package boltdb
 
 import (
+	"time"
+
+	"github.com/boltdb/bolt"
 	"github.com/btcboost/copernicus/orm"
 )
 
 type BoltDB struct {
 	orm.DBBase
+	*bolt.DB
+	filePath string
 }
 
 func NewBlotDB() orm.DBBase {
-	db := new(BoltDB)
-	return db
+	boltdb := new(BoltDB)
+	return boltdb
 }
 
-func (db *BoltDB) Type() string {
+func (boltdb *BoltDB) Type() string {
 	return "boltdb"
 }
 
-func (db *BoltDB) Begin(writable bool) (orm.DBTx, error) {
+func (boltdb *BoltDB) Begin(writable bool) (orm.DBTx, error) {
 	return nil, nil
 
 }
 
-func (db *BoltDB) View(fn func(tx orm.DBTx) error) error {
+func (boltdb *BoltDB) View(fn func(tx orm.DBTx) error) error {
 	return nil
 }
 
-func (db *BoltDB) Update(fn func(tx orm.DBTx) error) error {
+func (boltdb *BoltDB) Update(fn func(tx orm.DBTx) error) error {
 	return nil
 }
 
-func (db *BoltDB) Close() error {
+func (boltdb *BoltDB) Open() error {
+	db, err := bolt.Open(boltdb.filePath, 0600, &bolt.Options{Timeout: 1 * time.Second})
+	if err != nil {
+		return err
+	}
+	boltdb.DB = db
 	return nil
+}
+
+func (boltdb *BoltDB) Close() error {
+	return boltdb.DB.Close()
 }
