e1547c6e805048852d5395109102869cf05a3336
jjz <woaf1003@gmail.com>
2018-01-27 18:01:30 +0800

    realize the cursor

--- boltdb/bucket.go
+++ boltdb/bucket.go
@@ -10,26 +10,40 @@ type bucket struct {
 	boltBucket *bolt.Bucket
 }
 
-func (bucket *bucket) ForEach(func(k, v []byte) error) error {
+func (bucket *bucket) ForEach(fn func(key, value []byte) error) error {
+	c := bucket.boltBucket.Cursor()
+	for k, v := c.First(); k != nil; k, v = c.Next() {
+		err := fn(k, v)
+		if err != nil {
+			return err
+		}
+	}
 	return nil
 }
 
 func (bucket *bucket) Cursor() orm.Cursor {
-	return nil
+	c := bucket.boltBucket.Cursor()
+	cursor := new(cursor)
+	cursor.boltCursor = c
+	cursor.bucket = bucket
+	return cursor
 }
 
 func (bucket *bucket) Writable() bool {
-	return false
+	return bucket.Bucket.Writable()
 }
 
 func (bucket *bucket) Put(key, value []byte) error {
-	return nil
+	err := bucket.Bucket.Put(key, value)
+	return err
 }
 
 func (bucket *bucket) Get(key []byte) []byte {
-	return nil
+	return bucket.Bucket.Get(key)
+
 }
 
 func (bucket *bucket) Delete(key []byte) error {
-	return nil
+	err := bucket.Bucket.Delete(key)
+	return err
 }
