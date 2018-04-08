268dc2d7aa2600e672081304c9e0a11c1d04a44c
jjz <woaf1003@gmail.com>
2018-04-08 21:40:08 +0800

    start PeerManager use lvldb

--- main.go
+++ main.go
@@ -3,6 +3,12 @@ package main
 // please ensure init `github.com/btcboost/copernicus/log` firstly,
 // or you will get an error log output.
 import (
+	"fmt"
+	"github.com/btcboost/copernicus/conf"
+	"github.com/btcboost/copernicus/database"
+	"github.com/btcboost/copernicus/net/msg"
+	"github.com/btcboost/copernicus/net/p2p"
+	"github.com/btcboost/copernicus/utils"
 	"os"
 	"syscall"
 
@@ -30,23 +36,36 @@ func main() {
 }
 
 func startBitcoin() error {
-	// db, err := database.InitDB(database.DBBolt, conf.AppConf.DataDir+"/peer-")
-	// if err != nil {
-	// 	log.Error("InitDB:", err.Error())
-	// 	return err
-	// }
-	// peerManager, err := p2p.NewPeerManager(conf.AppConf.Listeners, db, msg.ActiveNetParams)
-	// if err != nil {
-	// 	log.Error("unable to start server on %v:%v", conf.AppConf.Listeners, err)
-	// 	return err
-	// }
-	// defer func() {
-	// 	log.Info("gracefully shutting down the server ....")
-	// 	peerManager.Stop()
-	// 	peerManager.WaitForShutdown()
-	// 	log.Info("server shutdown complete")
-	// }()
-	//
-	// peerManager.Start()
+	path := conf.AppConf.DataDir + "/peer"
+	exists := utils.PathExists(path)
+	if !exists {
+		utils.MakePath(path)
+	}
+	db, err := database.NewDBWrapper(&database.DBOption{
+		FilePath:      path,
+		CacheSize:     1 << 20,
+		DontObfuscate: false,
+	})
+	if err != nil {
+		fmt.Println("InitDB:", err.Error())
+		return err
+	}
+	defer db.Close()
+	fmt.Println("InitDB finish")
+	peerManager, err := p2p.NewPeerManager(conf.AppConf.Listeners, *db, msg.ActiveNetParams)
+	if err != nil {
+		fmt.Printf("unable to start server on %v:%v \n", conf.AppConf.Listeners, err)
+		return err
+	}
+	fmt.Println("PeerManager Init")
+	//defer func() {
+	//	fmt.Println("gracefully shutting down the server ....")
+	//	peerManager.Stop()
+	//	peerManager.WaitForShutdown()
+	//	fmt.Println("server shutdown complete")
+	//}()
+
+	peerManager.Start()
+
 	return nil
 }
