42085d3eda76d2f8f433a44b710cdc8f96398a58
ludete <yyxyong@163.com>
2018-04-11 21:14:03 +0800

    add TxEntry state, TxMempool state, And implement RootTx logic

--- mempool/txentry.go
+++ mempool/txentry.go
@@ -1,34 +1,31 @@
 package mempool
 
 import (
-	"time"
 	"unsafe"
 
 	"github.com/btcboost/copernicus/core"
-	"github.com/btcboost/copernicus/utils"
 	"github.com/google/btree"
 )
 
 // TxEntry are not safe for concurrent write and read access .
 type TxEntry struct {
-	tx       *core.Tx
-	txHeight int
-	txSize   int
+	tx     *core.Tx
+	txSize int
 	// txFee tis transaction fee
-	txFee     int64
-	txFeeRate utils.FeeRate
-	// sumTxCount is this tx and all Descendants transaction's number.
-	sumTxCountWithDescendants uint64
-	// sumFee is calculated by this tx and all Descendants transaction;
+	txFee int64
+	// sumTxCountWithDescendants is this tx and all Descendants transaction's number. init number is 1.
+	sumTxCountWithDescendants int64
+	// sumFeeWithDescendants is calculated by this tx and all Descendants transaction;
 	sumFeeWithDescendants int64
-	// sumSize size calculated by this tx and all Descendants transaction;
-	sumSizeWithDescendants uint64
+	// sumSizeWithDescendants size calculated by this tx and all Descendants transaction;
+	sumSizeWithDescendants int64
 
-	sumTxCountWithAncestors    uint64
-	sumSizeWitAncestors        uint64
-	sumSigOpCountWithAncestors uint64
-	// Total sigop plus P2SH sigops count
-	sigOpCount uint64
+	sumTxCountWithAncestors    int64
+	sumSizeWitAncestors        int64
+	sumSigOpCountWithAncestors int64
+	sumFeeWithAncestors        int64
+	// sigOpCount sigop plus P2SH sigops count
+	sigOpCount int
 	// time Local time when entering the memPool
 	time int64
 	// usageSize and total memory usage
@@ -43,27 +40,27 @@ type TxEntry struct {
 	spendsCoinbase bool
 }
 
-func (t *TxEntry) GetTxCountWithDescendants() uint64 {
+func (t *TxEntry) GetTxCountWithDescendants() int64 {
 	return t.sumTxCountWithDescendants
 }
 
-func (t *TxEntry) GetTxCountWithAncestors() uint64 {
+func (t *TxEntry) GetTxCountWithAncestors() int64 {
 	return t.sumTxCountWithAncestors
 }
 
-func (t *TxEntry) GetSizeWithAncestors() uint64 {
+func (t *TxEntry) GetSizeWithAncestors() int64 {
 	return t.sumSizeWitAncestors
 }
 
-func (t *TxEntry) GetSizeWithDescendants() uint64 {
+func (t *TxEntry) GetSizeWithDescendants() int64 {
 	return t.sumSizeWithDescendants
 }
 
-func (t *TxEntry) GetSigOpCount() uint64 {
+func (t *TxEntry) GetSigOpCount() int {
 	return t.sigOpCount
 }
 
-func (t *TxEntry) GetSigOpCountWithAncestors() uint64 {
+func (t *TxEntry) GetSigOpCountWithAncestors() int64 {
 	return t.sumSigOpCountWithAncestors
 }
 
@@ -92,31 +89,64 @@ func (t *TxEntry) GetSpendsCoinbase() bool {
 }
 
 // UpdateParent update the tx's parent transaction.
-func (t *TxEntry) UpdateParent(parent *TxEntry, add bool) {
+func (t *TxEntry) UpdateParent(parent *TxEntry, innerUsage *int64, add bool) {
 	if add {
 		t.parentTx[parent] = struct{}{}
+		*innerUsage += int64(unsafe.Sizeof(parent))
+		return
 	}
 	delete(t.parentTx, parent)
+	*innerUsage -= int64(unsafe.Sizeof(parent))
+}
+
+func (t *TxEntry) UpdateChild(child *TxEntry, innerUsage *int64, add bool) {
+	if add {
+		t.childTx[child] = struct{}{}
+		*innerUsage += int64(unsafe.Sizeof(child))
+		return
+	}
+	delete(t.childTx, child)
+	*innerUsage -= int64(unsafe.Sizeof(child))
+}
+
+func (t *TxEntry) UpdateDescendantState(updateCount, updateSize int, updateFee int64) {
+	t.sumTxCountWithDescendants += int64(updateCount)
+	t.sumSizeWithDescendants += int64(updateSize)
+	t.sumFeeWithDescendants += updateFee
+}
+
+func (t *TxEntry) UpdateAncestorState(updateCount, updateSize, updateSigOps int, updateFee int64) {
+	t.sumSizeWitAncestors += int64(updateSize)
+	t.sumTxCountWithAncestors += int64(updateCount)
+	t.sumSigOpCountWithAncestors += int64(updateSigOps)
+	t.sumFeeWithAncestors += updateFee
 }
 
 func (t *TxEntry) Less(than btree.Item) bool {
 	return t.time < than.(*TxEntry).time
 }
 
-func NewTxentry(tx *core.Tx, txFee int64) *TxEntry {
+func NewTxentry(tx *core.Tx, txFee int64, acceptTime int64, lp core.LockPoints, sigOpsCount int, spendCoinbase bool) *TxEntry {
 	t := new(TxEntry)
 	t.tx = tx
-	t.time = time.Now().Unix()
+	t.time = acceptTime
 	t.txSize = tx.SerializeSize()
-	t.sumSizeWithDescendants = uint64(t.txSize)
 	t.txFee = txFee
-	t.txFeeRate = utils.NewFeeRateWithSize(txFee, t.txSize)
-	t.usageSize = t.txSize + int(unsafe.Sizeof(t.txSize)*2+
-		unsafe.Sizeof(t.sumTxCountWithDescendants)*4+unsafe.Sizeof(t.txFeeRate))
+	t.usageSize = t.txSize + int(unsafe.Sizeof(t.lp))
+	t.spendsCoinbase = spendCoinbase
+	t.lp = lp
+
+	t.sumSizeWithDescendants = int64(t.txSize)
+	t.sumFeeWithDescendants = txFee
+	t.sumTxCountWithDescendants = 1
+
+	t.sumFeeWithAncestors = txFee
+	t.sumSizeWitAncestors = int64(t.txSize)
+	t.sumTxCountWithAncestors = 1
+	t.sumSigOpCountWithAncestors = int64(sigOpsCount)
+
 	t.parentTx = make(map[*TxEntry]struct{})
 	t.childTx = make(map[*TxEntry]struct{})
-	t.sumFeeWithDescendants += txFee
-	t.sumTxCountWithDescendants++
 
 	return t
 }
