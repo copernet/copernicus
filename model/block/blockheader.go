4d8c638cdc3a3dbb63eb3206445efd40957bd09b
yujian <yujian0231@163.com>
2018-05-10 10:19:42 +0800

    fix import path

--- model/block/blockheader.go
+++ model/block/blockheader.go
@@ -2,11 +2,11 @@ package block
 
 import (
 	"bytes"
-	"io"
 	"fmt"
+	"io"
 
+	"github.com/btcboost/copernicus/crypto"
 	"github.com/btcboost/copernicus/util"
-	"github.com/btcboost/copernicus/util/crypto"
 )
 
 type BlockHeader struct {
@@ -46,7 +46,7 @@ func (bh *BlockHeader) Serialize(w io.Writer) error {
 	return util.WriteElements(w, bh.Version, &bh.HashPrevBlock, &bh.MerkleRoot, bh.Time, bh.Bits, bh.Nonce)
 }
 
-func (bh *BlockHeader) Deserialize(r io.Reader) error {
+func (bh *BlockHeader) Unserialize(r io.Reader) error {
 	return util.ReadElements(r, &bh.Version, &bh.HashPrevBlock, &bh.MerkleRoot, &bh.Time, &bh.Bits, &bh.Nonce)
 }
 
