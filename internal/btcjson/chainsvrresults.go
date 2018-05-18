b199d3d62f235247dc3d75e436206e941e6e23bd
qshuai <qishuai231@gmail.com>
2018-05-18 16:43:40 +0800

    handleDecodeScript rpc command

--- internal/btcjson/chainsvrresults.go
+++ internal/btcjson/chainsvrresults.go
@@ -291,6 +291,7 @@ type ScriptPubKeyResult struct {
 	ReqSigs   int32    `json:"reqSigs,omitempty"`
 	Type      string   `json:"type"`
 	Addresses []string `json:"addresses,omitempty"`
+	P2SH      string   `json:"p2sh,omitempty"`
 }
 
 // GetTxOutResult models the data from the gettxout command.
