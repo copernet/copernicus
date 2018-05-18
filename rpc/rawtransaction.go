f2845ab99b1df7355644d9e1bfe41048cac84eac
ericnes <sl_jang@163.com>
2018-05-18 17:05:50 +0800

    rpc gettxoutproof

--- rpc/rawtransaction.go
+++ rpc/rawtransaction.go
@@ -18,7 +18,7 @@ import (
 	"github.com/btcboost/copernicus/model/tx"
 	"github.com/btcboost/copernicus/model/utxo"
 	"github.com/btcboost/copernicus/util"
-	"github.com/btcboost/copernicus/service"
+	"gopkg.in/fatih/set.v0"
 )
 
 var rawTransactionHandlers = map[string]commandHandler{
@@ -462,6 +462,38 @@ func handleSignRawTransaction(s *Server, cmd interface{}, closeChan <-chan struc
 }
 
 func handleGetTxoutProof(s *Server, cmd interface{}, closeChan <-chan struct{}) (interface{}, error) {
+	c := cmd.(*btcjson.GetTxOutProofCmd)
+	setTxIds := set.New()
+	var oneTxId util.Hash
+	txIds := c.TxIDs
+
+	for idx := 0; idx < len(txIds); idx++ {
+		txId := txIds[idx]
+		_, err := util.GetHashFromStr(txId)
+		if len(txId) != 64 || err == nil {
+			return nil, &btcjson.RPCError{
+				Code:    btcjson.ErrRPCInvalidParameter,
+				Message: "Invalid txid " + txId,
+			}
+		}
+
+        hash := util.HashFromString(txId)
+		if setTxIds.Has(hash) {
+			return nil, &btcjson.RPCError{
+				Code:    btcjson.ErrRPCInvalidParameter,
+				Message: "Invalid parameter, duplicated txid: " + txId,
+			}
+		}
+		setTxIds.Add(hash)
+		oneTxId = *hash
+	}
+
+	blkIndex := blockindex.BlockIndex{}
+	var hashBlk util.Hash
+	if c.BlockHash != nil {
+
+	}
+
 	return nil, nil
 }
 
