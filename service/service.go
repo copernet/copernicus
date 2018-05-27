45d1bd9ae1040a9ae010a51e33881184bd7ee216
hycxa <hycxa@163.com>
2018-05-27 16:17:03 +0800

    remove service rpc

--- service/service.go
+++ service/service.go
@@ -9,9 +9,6 @@ import (
 	//"fmt"
 
 	"github.com/btcboost/copernicus/log"
-	"github.com/btcboost/copernicus/model/block"
-	"github.com/btcboost/copernicus/model/tx"
-	"github.com/btcboost/copernicus/rpc/btcjson"
 	//"github.com/btcboost/copernicus/model/block"
 	//"github.com/btcboost/copernicus/model/tx"
 	"github.com/btcboost/copernicus/net/server"
@@ -160,67 +157,67 @@ out:
 
 // Rpc process things
 func ProcessForRpc(message interface{}) (rsp interface{}, err error) {
-	switch m := message.(type) {
-
-	case *GetConnectionCountRequest:
-		return msgHandle.server.ConnectedCount(), nil
-
-	case *wire.MsgPing:
-		return msgHandle.server.BroadcastMessage(), nil
-
-	case *GetPeersInfoRequest:
-		return nil, nil
-
-	case *btcjson.AddNodeCmd:
-		err = msgHandle.NodeOpera(m)
-		return
-
-	case *btcjson.DisconnectNodeCmd:
-		return
-
-	case *btcjson.GetAddedNodeInfoCmd:
-		return msgHandle.connManager.PersistentPeers(), nil
-
-	case *GetNetTotalsRequest:
-		return
-
-	case *btcjson.GetNetworkInfoCmd:
-		return
-
-	case *btcjson.SetBanCmd:
-		return
-
-	case *ListBannedRequest:
-		return
-
-	case *ClearBannedRequest:
-		return
-
-	case *wire.InvVect:
-		// todo
-		return
-
-	case *tx.Tx:
-		msgHandle.recvChannel <- m
-		ret := <-msgHandle.resultChannel
-		switch r := ret.(type) {
-		case error:
-			return nil, r
-		case []*tx.Tx:
-			return r, nil
-		}
-
-	case *block.Block:
-		msgHandle.recvChannel <- m
-		ret := <-msgHandle.resultChannel
-		switch r := ret.(type) {
-		case error:
-			return nil, r
-		case BlockState:
-			return r, nil
-		}
-
-	}
+	// switch m := message.(type) {
+	//
+	// case *GetConnectionCountRequest:
+	// 	return msgHandle.server.ConnectedCount(), nil
+	//
+	// case *wire.MsgPing:
+	// 	return msgHandle.server.BroadcastMessage(), nil
+	//
+	// case *GetPeersInfoRequest:
+	// 	return nil, nil
+	//
+	// case *btcjson.AddNodeCmd:
+	// 	err = msgHandle.NodeOpera(m)
+	// 	return
+	//
+	// case *btcjson.DisconnectNodeCmd:
+	// 	return
+	//
+	// case *btcjson.GetAddedNodeInfoCmd:
+	// 	return msgHandle.connManager.PersistentPeers(), nil
+	//
+	// case *GetNetTotalsRequest:
+	// 	return
+	//
+	// case *btcjson.GetNetworkInfoCmd:
+	// 	return
+	//
+	// case *btcjson.SetBanCmd:
+	// 	return
+	//
+	// case *ListBannedRequest:
+	// 	return
+	//
+	// case *ClearBannedRequest:
+	// 	return
+	//
+	// case *wire.InvVect:
+	// 	// todo
+	// 	return
+	//
+	// case *tx.Tx:
+	// 	msgHandle.recvChannel <- m
+	// 	ret := <-msgHandle.resultChannel
+	// 	switch r := ret.(type) {
+	// 	case error:
+	// 		return nil, r
+	// 	case []*tx.Tx:
+	// 		return r, nil
+	// 	}
+	//
+	// case *block.Block:
+	// 	msgHandle.recvChannel <- m
+	// 	ret := <-msgHandle.resultChannel
+	// 	switch r := ret.(type) {
+	// 	case error:
+	// 		return nil, r
+	// 	case BlockState:
+	// 		return r, nil
+	// 	}
+	//
+	// }
 
 	return nil, errors.New("unknown rpc request")
 }
