b5dcf1e141bbb63faa3a22068fed441f09cca3c9
wolf4j <rxl5555555@qq.com>
2018-07-09 15:43:40 +0800

    add unit test of lrucache

--- model/utxo/lrucache_test.go
+++ model/utxo/lrucache_test.go
@@ -22,11 +22,11 @@ func TestLRUCache(t *testing.T) {
 	hash1 := util.HashFromString("000000002dd5588a74784eaa7ab0507a18ad16a236e7b1ce69f00d7ddfb5d0a6")
 	outpoint1 := outpoint.OutPoint{Hash: *hash1, Index: 0}
 
-	script2 := script.NewScriptRaw([]byte{opcodes.OP_11, opcodes.OP_EQUAL})
-	txout2 := txout.NewTxOut(3, script2)
+	script1 := script.NewScriptRaw([]byte{opcodes.OP_11, opcodes.OP_EQUAL})
+	txout1 := txout.NewTxOut(3, script1)
 
 	necm.cacheCoins[outpoint1] = &Coin{
-		txOut:         *txout2,
+		txOut:         *txout1,
 		height:        10000,
 		isCoinBase:    false,
 		isMempoolCoin: false,
@@ -47,7 +47,7 @@ func TestLRUCache(t *testing.T) {
 	}
 
 	c := utxoTip.GetCoin(&outpoint1)
-	if c == nil  {
+	if c == nil {
 		t.Error("get coin faild...")
 	}
 
@@ -55,4 +55,40 @@ func TestLRUCache(t *testing.T) {
 	if !hv {
 		t.Error("the cache not have coin, please check...")
 	}
+
+	hash2 := util.HashFromString("000000002dd5588a74784eaa7ab0507a18ad16a236e7b1ce69f00d7ddfb5d0b7")
+	outpoint2 := outpoint.OutPoint{Hash: *hash2, Index: 0}
+
+	script2 := script.NewScriptRaw([]byte{opcodes.OP_12, opcodes.OP_EQUAL})
+	txout2 := txout.NewTxOut(3, script2)
+
+	necm.cacheCoins[outpoint2] = &Coin{
+		txOut:         *txout2,
+		height:        1 << 20,
+		isCoinBase:    false,
+		isMempoolCoin: false,
+		dirty:         false,
+		fresh:         false,
+	}
+
+	necm.AddCoin(&outpoint2, necm.cacheCoins[outpoint2])
+
+	ok2 := necm.Flush(*hash2)
+	if !ok2 {
+		t.Error("flush failed....", )
+	}
+
+	c2 := utxoTip.GetCoin(&outpoint2)
+	if c2 == nil {
+		t.Error("get coin faild...")
+	}
+	
+	clc := utxoTip.(*CoinsLruCache)
+	clc.UnCache(&outpoint2)
+
+	hv2 := utxoTip.HaveCoin(&outpoint2)
+	if !hv2 {
+		t.Error("the cache should not have coin, please check...")
+	}
+
 }
