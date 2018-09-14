b163dd3f1fcddb3a4df35d6a8b013245a7a6d157
jjz <woaf1003@gmail.com>
2018-09-14 20:54:38 +0800

    fix test failed

--- persist/blkdb/blkdb_test.go
+++ persist/blkdb/blkdb_test.go
@@ -105,47 +105,82 @@ func TestWriteReindexing(t *testing.T) {
 	}
 }
 
-//todo  test faileds
-
 func TestReadBlockFileInfo(t *testing.T) {
 	initBlockDB()
 	//the block file info exist
-	//bfi, err := GetInstance().ReadBlockFileInfo(0)
-	//if err != nil {
-	//	t.Error("read block file info<000> failed.")
-	//}
-	//log.Info("blockFileInfo value is:%v", bfi)
-	//if bfi == nil {
-	//	t.Errorf("the block fileInfo not equal nil:%v", bfi)
-	//}
-	//
-	////the block file info not exist
-	//bfi, err = GetInstance().ReadBlockFileInfo(1111)
-	//if err == nil {
-	//	t.Error("read block file info<1111> failed.")
-	//}
-	//if bfi != nil {
-	//	t.Error("error")
-	//}
+	_, err := writeBlockFile()
+	if err != nil {
+		t.Error("write block file ", err)
+	}
+	bfi, err := GetInstance().ReadBlockFileInfo(1)
+	if err != nil {
+		t.Error("read block file info<000> failed.", err)
+	}
+	log.Info("blockFileInfo value is:%v", bfi)
+	if bfi == nil {
+		t.Errorf("the block fileInfo not equal nil:%v", bfi)
+	}
+
+	//the block file info not exist
+	bfi, err = GetInstance().ReadBlockFileInfo(1111)
+	if err == nil {
+		t.Error("read block file info<1111> failed.")
+	}
+	if bfi != nil {
+		t.Error("error")
+	}
 }
 
-//todo test failed
+func writeBlockFile() (*util.Hash, error) {
+
+	blkHeader := block.NewBlockHeader()
+	bfi1 := make(map[int32]*block.BlockFileInfo)
+	fi := block.NewBlockFileInfo()
+	fi.UndoSize = 0
+	fi.Size = 1
+	fi.Blocks = 1
+	fi.HeightFirst = 1
+	fi.HeightLast = 2
+	bfi1[1] = fi
+
+	//init block header
+	blkHeader1 := block.NewBlockHeader()
+	blkHeader.Time = uint32(1534822771)
+	blkHeader.Version = 536870912
+	blkHeader.Bits = 486604799
+	preHash := util.HashFromString("00000000000001bcd6b635a1249dfbe76c0d001592a7219a36cd9bbd002c7238")
+	merkleRoot := util.HashFromString("7e814211a7de289a490380c0c20353e0fd4e62bf55a05b38e1628e0ea0b4fd3d")
+	blkHeader.HashPrevBlock = *preHash
+	blkHeader.Nonce = 1391785674
+	blkHeader.MerkleRoot = *merkleRoot
+	//init block index
+	blkidx := blockindex.NewBlockIndex(blkHeader1)
+	blockHash := blkidx.GetBlockHash() //14508459b221041eab257d2baaa7459775ba748246c8403609eb708f0e57e74b
+	blkidxs := make([]*blockindex.BlockIndex, 0, 10)
+	blkidxs = append(blkidxs, blkidx)
+	err := GetInstance().WriteBatchSync(bfi1, 1, blkidxs)
+	return blockHash, err
+}
 
 func TestReadLastBlockFile(t *testing.T) {
 	initBlockDB()
-	//lastFile, err := GetInstance().ReadLastBlockFile()
-	//if err != nil {
-	//	t.Error("read last block file failed")
-	//}
-	//
-	//bfi, err := GetInstance().ReadBlockFileInfo(lastFile)
-	//if err != nil {
-	//	t.Error("read last block fileInfo failed")
-	//}
-	//log.Info("last blockFileInfo value is:%v", bfi)
-	//if bfi == nil {
-	//	t.Errorf("the last blockFileInfo not equal nil, the value is:%v", bfi)
-	//}
+	_, err := writeBlockFile()
+	if err != nil {
+		t.Errorf("write blockFileInfo failed.")
+	}
+	lastFile, err := GetInstance().ReadLastBlockFile()
+	if err != nil {
+		t.Error("read last block file failed -", err)
+	}
+
+	bfi, err := GetInstance().ReadBlockFileInfo(lastFile)
+	if err != nil {
+		t.Error("read last block fileInfo failed", err)
+	}
+	log.Info("last blockFileInfo value is:%v", bfi)
+	if bfi == nil {
+		t.Errorf("the last blockFileInfo not equal nil, the value is:%v", bfi)
+	}
 }
 
 func TestWriteBatchSync(t *testing.T) {
