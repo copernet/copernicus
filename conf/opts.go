0709188dc4d5b2cf260aeb6490ae7b8da79b7933
jjz <woaf1003@gmail.com>
2018-09-23 18:37:30 +0800

    add opts_test.go

--- conf/opts.go
+++ conf/opts.go
@@ -3,17 +3,16 @@ package conf
 import (
 	"fmt"
 	"github.com/jessevdk/go-flags"
-	"os"
 )
 
 type Opts struct {
 	DataDir  string `long:"datadir" description:"specified program data dir"`
-	Discover int   `long:"discover" description:"Discover own IP addresses (default: 1 when listening and no -externalip or -proxy)"`
+	Discover int    `long:"discover" description:"Discover own IP addresses (default: 1 when listening and no -externalip or -proxy)"`
 }
 
-func InitArgs() *Opts {
+func InitArgs(args []string) *Opts {
 	opts := new(Opts)
-	_, err := flags.ParseArgs(opts, os.Args)
+	_, err := flags.ParseArgs(opts, args)
 	if err != nil {
 		panic(err)
 	}
