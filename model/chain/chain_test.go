40260ce1fcd595235fde21490ef67b781bddb091
georgefff <514625951@qq.com>
2018-10-23 03:23:39 +0800

    add model chain test

--- model/chain/chain_test.go
+++ model/chain/chain_test.go
@@ -3,6 +3,7 @@ package chain
 import (
 	"github.com/copernet/copernicus/model/blockindex"
 	"github.com/copernet/copernicus/persist"
+	"gopkg.in/fatih/set.v0"
 	"os"
 
 	"github.com/copernet/copernicus/conf"
@@ -14,8 +15,6 @@ import (
 	"math/big"
 	"testing"
 )
-
-var testChain *Chain
 
 func TestMain(m *testing.M) {
 	persist.InitPersistGlobal()
@@ -29,164 +28,230 @@ func getBlockIndex(indexPrev *blockindex.BlockIndex, timeInterval int64, bits ui
 	blockIdx.Height = indexPrev.Height + 1
 	blockIdx.Header.Time = indexPrev.Header.Time + uint32(timeInterval)
 	blockIdx.Header.Bits = bits
+	blockIdx.Header.HashPrevBlock = indexPrev.Header.GetHash()
 	blockIdx.ChainWork = *big.NewInt(0).Add(&indexPrev.ChainWork, pow.GetBlockProof(blockIdx))
 	return blockIdx
 }
 
-func TestChain(t *testing.T) {
+func TestChain_Simple(t *testing.T) {
 	InitGlobalChain()
-	testChain = GetInstance()
-	testChain.indexMap = make(map[util.Hash]*blockindex.BlockIndex)
-	blockIdx := make([]*blockindex.BlockIndex, 50)
-	initBits := model.ActiveNetParams.PowLimitBits
-	timePerBlock := int64(model.ActiveNetParams.TargetTimePerBlock)
-	height := 0
+	tChain := GetInstance()
 
-	// Pile up some blocks.
-	blockIdx[height] = blockindex.NewBlockIndex(&model.ActiveNetParams.GenesisBlock.Header)
-	testChain.AddToBranch(blockIdx[0])
-	testChain.AddToIndexMap(blockIdx[0])
-	testChain.active = append(testChain.active, blockIdx[0])
-
-	for height = 1; height < 11; height++ {
-		blockIdx[height] = getBlockIndex(blockIdx[height-1], timePerBlock, initBits)
-		testChain.AddToBranch(blockIdx[height])
-		testChain.AddToIndexMap(blockIdx[height])
-		testChain.active = append(testChain.active, blockIdx[height])
-	}
-	for height = 11; height < 16; height++ {
-		blockIdx[height] = getBlockIndex(blockIdx[height-1], timePerBlock, initBits)
-		testChain.AddToBranch(blockIdx[height])
-		testChain.AddToIndexMap(blockIdx[height])
-	}
-
-	if testChain.GetParams() != model.ActiveNetParams {
-		t.Errorf("GetParams "+
-			"expect: %s, actual: %s", model.ActiveNetParams.Name, testChain.GetParams().Name)
-	}
-	if testChain.Genesis() != blockIdx[0] {
-		t.Errorf("Genesis "+
-			"expect: %s, actual: %s", blockIdx[0].GetBlockHash(), testChain.Genesis().GetBlockHash())
-	}
-	for i := 0; i < 11; i++ {
-		hash := blockIdx[i].GetBlockHash()
-		actualBlockIdx := testChain.FindHashInActive(*hash)
-		if actualBlockIdx != blockIdx[i] {
-			t.Errorf("FindHashInActive "+
-				"expect: %s, actual: %s", blockIdx[i].GetBlockHash(), actualBlockIdx.GetBlockHash())
-		}
-	}
-	for i := 11; i < 16; i++ {
-		hash := blockIdx[i].GetBlockHash()
-		actualBlockIdx := testChain.FindHashInActive(*hash)
-		if actualBlockIdx != nil {
-			t.Errorf("active chain should not have this blockindex, pls check")
-		}
-	}
-	for i := 0; i < 16; i++ {
-		hash := blockIdx[i].GetBlockHash()
-		actualBlockIdx := testChain.FindBlockIndex(*hash)
-		if actualBlockIdx != blockIdx[i] {
-			t.Errorf("FindBlockIndex "+
-				"expect: %s, actual: %s", blockIdx[i].GetBlockHash(), actualBlockIdx.GetBlockHash())
-		}
-	}
-	if testChain.Tip() != blockIdx[10] {
-		t.Errorf("Tip "+
-			"expect: %s, actual: %s", blockIdx[10].GetBlockHash(), testChain.Tip().GetBlockHash())
-	}
-	if testChain.TipHeight() != 10 {
-		t.Errorf("TipHeight "+
-			"expect: 10, actual: %d", testChain.TipHeight())
-	}
-	for i := 0; i < 16; i++ {
-		hash := blockIdx[i].GetBlockHash()
-		actualHeight := testChain.GetSpendHeight(hash)
-		if actualHeight != -1 && actualHeight != blockIdx[i].Height+1 {
-			t.Errorf("GetSpendHeight "+
-				"expect: %d, actual: %d", blockIdx[i].Height+1, actualHeight)
-		}
-	}
-	for i := 0; i < 11; i++ {
-		actualBlockIdx := testChain.GetIndex(int32(i))
-		if actualBlockIdx != blockIdx[i] {
-			t.Errorf("GetIndex "+
-				"expect: %s, actual: %s", blockIdx[i].GetBlockHash(), actualBlockIdx.GetBlockHash())
-		}
-	}
-	for i := 11; i < 20; i++ {
-		actualBlockIdx := testChain.GetIndex(int32(i))
-		if actualBlockIdx != nil {
-			t.Errorf("active chain should not have this blockindex, pls check")
-		}
-	}
-	for i := 0; i < 11; i++ {
-		if !testChain.Contains(blockIdx[i]) {
-			t.Errorf("active chain should contains this blockindex, pls check")
-		}
-	}
-	for i := 11; i < 20; i++ {
-		if testChain.Contains(blockIdx[i]) {
-			t.Errorf("active chain should not contains this blockindex, pls check")
-		}
-	}
-	if testChain.FindFork(blockIdx[15]) != blockIdx[10] {
-		t.Errorf("FindFork "+
-			"expect: %s, actual: %s", blockIdx[10].GetBlockHash(), testChain.FindFork(blockIdx[15]).GetBlockHash())
-	}
-	for i := 0; i < 16; i++ {
-		if !testChain.InBranch(blockIdx[i]) {
-			t.Errorf("branch should contains this blockindex, pls check")
-		}
-	}
-	if testChain.FindMostWorkChain() != blockIdx[15] {
-		t.Errorf("FindMostWorkChain "+
-			"expect: %s, actual: %s", blockIdx[15].GetBlockHash(), testChain.FindMostWorkChain().GetBlockHash())
-	}
-	if testChain.SetTip(blockIdx[14]); testChain.Tip() != blockIdx[14] {
-		t.Errorf("SetTip "+
-			"expect: %s, actual: %s", blockIdx[14].GetBlockHash(), testChain.Tip().GetBlockHash())
-	}
-
-}
-
-func TestChain_AddToBranch(t *testing.T) {
-	InitGlobalChain()
-	testChain = GetInstance()
-	testChain.indexMap = make(map[util.Hash]*blockindex.BlockIndex)
-	blockIdx := make([]*blockindex.BlockIndex, 50)
+	tChain.indexMap = make(map[util.Hash]*blockindex.BlockIndex)
+	bIndex := make([]*blockindex.BlockIndex, 50)
+	tChain.active = make([]*blockindex.BlockIndex, 0)
+	tChain.branch = make([]*blockindex.BlockIndex, 0)
 	initBits := model.ActiveNetParams.PowLimitBits
 	timePerBlock := int64(model.ActiveNetParams.TargetTimePerBlock)
 	height := 0
 
 	//Pile up some blocks
-	blockIdx[height] = blockindex.NewBlockIndex(&model.ActiveNetParams.GenesisBlock.Header)
-	testChain.AddToBranch(blockIdx[0])
+	bIndex[0] = blockindex.NewBlockIndex(&model.ActiveNetParams.GenesisBlock.Header)
+	tChain.AddToBranch(bIndex[0])
+	tChain.AddToIndexMap(bIndex[0])
+	tChain.active = append(tChain.active, bIndex[0])
+
 	for height = 1; height < 11; height++ {
-		i := height
-		dummyPow := big.NewInt(0).Rsh(model.ActiveNetParams.PowLimit, uint(i))
-		blockIdx[height] = getBlockIndex(blockIdx[height-1], timePerBlock, pow.BigToCompact(dummyPow))
-		testChain.AddToBranch(blockIdx[height])
+		bIndex[height] = getBlockIndex(bIndex[height-1], timePerBlock, initBits)
+		tChain.AddToBranch(bIndex[height])
+		tChain.AddToIndexMap(bIndex[height])
+		tChain.active = append(tChain.active, bIndex[height])
 	}
-	for height = 11; height < 21; height++ {
-		blockIdx[height] = getBlockIndex(blockIdx[height-11], timePerBlock, initBits)
-		testChain.AddToBranch(blockIdx[height])
+	for height = 11; height < 16; height++ {
+		bIndex[height] = getBlockIndex(bIndex[height-1], timePerBlock, initBits)
+		tChain.AddToBranch(bIndex[height])
+		tChain.AddToIndexMap(bIndex[height])
 	}
 
-	if len(testChain.branch) != 26 {
-		t.Errorf("some block does not addtobranch, pls check")
+	if tChain.GetParams() != model.ActiveNetParams {
+		t.Errorf("GetParams expect: %s, actual: %s", model.ActiveNetParams.Name, tChain.GetParams().Name)
 	}
-	if testChain.FindMostWorkChain() != blockIdx[10] {
-		t.Errorf("some block does not sort by work, pls check")
+	if tChain.Genesis() != bIndex[0] {
+		t.Errorf("Genesis expect: %s, actual: %s", bIndex[0].GetBlockHash(), tChain.Genesis().GetBlockHash())
 	}
-	if testChain.RemoveFromBranch(blockIdx[0]); testChain.InBranch(blockIdx[0]) {
-		t.Errorf("block should not in branch")
+	if tChain.FindHashInActive(model.GenesisBlockHash) != bIndex[0] {
+		t.Errorf("FindHashInActive Error")
+	}
+	if tChain.FindBlockIndex(model.GenesisBlockHash) != bIndex[0] {
+		t.Errorf("FindHashInActive Error")
+	}
+	if tChain.Tip() != bIndex[10] {
+		t.Errorf("Tip Error")
+	}
+	if tChain.TipHeight() != 10 {
+		t.Errorf("TipHeight Error")
+	}
+	if tChain.IsCurrent() {
+		t.Errorf("IsCurrent Error")
+	}
+	if tChain.GetSpendHeight(bIndex[15].GetBlockHash()) != 16 {
+		t.Errorf("GetSpendHeight Error")
+	}
+	if tChain.GetIndex(10) != bIndex[10] {
+		t.Errorf("GetIndex Error")
+	}
+	if !tChain.Equal(tChain) {
+		t.Errorf("Equal Error")
+	}
+	if tChain.Contains(bIndex[15]) {
+		t.Errorf("Contains Error")
+	}
+	if tChain.Next(bIndex[9]) != bIndex[10] {
+		t.Errorf("Next Error")
+	}
+	if tChain.Height() != 10 {
+		t.Errorf("Height Error")
+	}
+	if tChain.GetAncestor(10) != bIndex[10] {
+		t.Errorf("GetAncestor Error")
+	}
+	if tChain.SetTip(bIndex[6]); tChain.Tip() != bIndex[6] {
+		t.Errorf("SetTip Error")
+	}
+	if !tChain.ParentInBranch(bIndex[10]) {
+		t.Errorf("ParentInBranch Error")
+	}
+	if tChain.RemoveFromBranch(bIndex[15]); tChain.InBranch(bIndex[15]) {
+		t.Errorf("InBranch Error")
+	}
+	if tChain.FindMostWorkChain() != bIndex[14] {
+		t.Errorf("FindMostWorkChain Error")
+	}
+	if tChain.AddToOrphan(bIndex[15]); tChain.ChainOrphanLen() != 1 {
+		t.Errorf("AddToOrphan Error")
+	}
+	if tChain.IndexMapSize() != 16 {
+		t.Errorf("IndexMapSize Error")
+	}
+	if tChain.ClearActive(); tChain.Tip() != nil {
+		t.Errorf("ClearActive Error")
+	}
+
+}
+
+func TestChain_Fork(t *testing.T) {
+	InitGlobalChain()
+	tChain := GetInstance()
+
+	tChain.indexMap = make(map[util.Hash]*blockindex.BlockIndex)
+	tChain.active = make([]*blockindex.BlockIndex, 0)
+	tChain.branch = make([]*blockindex.BlockIndex, 0)
+	bIndex := make([]*blockindex.BlockIndex, 50)
+	initBits := model.ActiveNetParams.PowLimitBits
+	timePerBlock := int64(model.ActiveNetParams.TargetTimePerBlock)
+	height := 0
+
+	//Pile up some blocks
+	bIndex[0] = blockindex.NewBlockIndex(&model.ActiveNetParams.GenesisBlock.Header)
+	tChain.AddToIndexMap(bIndex[0])
+	tChain.AddToBranch(bIndex[0])
+	tChain.active = append(tChain.active, bIndex[0])
+
+	for height = 1; height < 11; height++ {
+		bIndex[height] = getBlockIndex(bIndex[height-1], timePerBlock, initBits)
+		tChain.AddToBranch(bIndex[height])
+		tChain.AddToIndexMap(bIndex[height])
+		tChain.active = append(tChain.active, bIndex[height])
+	}
+	for height = 5; height < 15; height++ {
+		bIndex[height] = getBlockIndex(bIndex[height-1], timePerBlock-1, initBits)
+		tChain.AddToBranch(bIndex[height])
+		tChain.AddToIndexMap(bIndex[height])
+		tChain.active = append(tChain.active, bIndex[height])
+	}
+
+	if tChain.FindFork(bIndex[9]) != bIndex[4] {
+		t.Errorf("FindFork Error")
+	}
+
+	setTips := set.New()
+	setTips.Add(tChain.Tip())
+
+	if !tChain.GetChainTips().IsEqual(setTips) {
+		t.Errorf("GetChainTips Error")
+	}
+
+}
+
+func TestChain_InitLoad(t *testing.T) {
+	InitGlobalChain()
+	tChain := GetInstance()
+
+	tChain.indexMap = make(map[util.Hash]*blockindex.BlockIndex)
+	bIndex := make([]*blockindex.BlockIndex, 50)
+	tChain.active = make([]*blockindex.BlockIndex, 0)
+	tChain.branch = make([]*blockindex.BlockIndex, 0)
+	initBits := model.ActiveNetParams.PowLimitBits
+	timePerBlock := int64(model.ActiveNetParams.TargetTimePerBlock)
+	height := 0
+
+	tChain.InitLoad(tChain.indexMap, tChain.branch)
+	if tChain.Genesis() != nil {
+		t.Errorf("Genesis Error")
+	}
+	if tChain.FindHashInActive(model.GenesisBlockHash) != nil {
+		t.Errorf("FindHashInActive Error")
+	}
+	if tChain.FindBlockIndex(model.GenesisBlockHash) != nil {
+		t.Errorf("FindBlockIndex Error")
+	}
+	if tChain.Tip() != nil {
+		t.Errorf("Tip Error")
+	}
+	if tChain.TipHeight() != 0 {
+		t.Errorf("TipHeight Error")
+	}
+	if tChain.GetSpendHeight(&model.GenesisBlockHash) != -1 {
+		t.Errorf("GetSpendHeight Error")
+	}
+	if tChain.Contains(nil) {
+		t.Errorf("Contains Error")
+	}
+	if tChain.Next(nil) != nil {
+		t.Errorf("Next Error")
+	}
+	if tChain.FindFork(nil) != nil {
+		t.Errorf("FindFork Error")
+	}
+	if tChain.ParentInBranch(nil) {
+		t.Errorf("ParentInBranch Error")
+	}
+	if tChain.InBranch(nil) {
+		t.Errorf("InBranch Error")
+	}
+	if tChain.AddToBranch(nil) == nil {
+		t.Errorf("AddToBranch Error")
+	}
+	if tChain.RemoveFromBranch(nil) == nil {
+		t.Errorf("RemoveFromBranch Error")
+	}
+	if tChain.FindMostWorkChain() != nil {
+		t.Errorf("FindMostWorkChain Error")
+	}
+	if tChain.AddToIndexMap(nil) == nil {
+		t.Errorf("AddToIndexMap Error")
+	}
+
+	//Pile up some blocks
+	bIndex[0] = blockindex.NewBlockIndex(&model.ActiveNetParams.GenesisBlock.Header)
+	tChain.AddToBranch(bIndex[0])
+	tChain.AddToIndexMap(bIndex[0])
+	tChain.active = append(tChain.active, bIndex[0])
+
+	for height = 1; height < 11; height++ {
+		bIndex[height] = getBlockIndex(bIndex[height-1], timePerBlock, initBits)
+		tChain.AddToBranch(bIndex[height])
+		tChain.AddToIndexMap(bIndex[height])
+		tChain.active = append(tChain.active, bIndex[height])
+	}
+	if tChain.SetTip(nil); tChain.Tip() != nil {
+		t.Errorf("SetTip Error")
 	}
 }
 
 func TestChain_GetBlockScriptFlags(t *testing.T) {
 	InitGlobalChain()
-	testChain = GetInstance()
+	testChain := GetInstance()
 	timePerBlock := int64(model.ActiveNetParams.TargetTimePerBlock)
 	initBits := model.ActiveNetParams.PowLimitBits
 
@@ -246,7 +311,7 @@ func TestChain_GetBlockScriptFlags(t *testing.T) {
 func TestBuildForwardTree(t *testing.T) {
 	globalChain = nil
 	InitGlobalChain()
-	testChain = GetInstance()
+	testChain := GetInstance()
 	testChain.indexMap = make(map[util.Hash]*blockindex.BlockIndex)
 	blockIdx := make([]*blockindex.BlockIndex, 50)
 	initBits := model.ActiveNetParams.PowLimitBits
