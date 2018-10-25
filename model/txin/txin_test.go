34c1ff987dc8f037303c1dc5b7a47cd9b8b91f89
wonghoifung <wonghoifung85@gmail.com>
2018-10-25 13:37:35 +0800

    txin unit test 100% coverage

--- model/txin/txin_test.go
+++ model/txin/txin_test.go
@@ -5,9 +5,41 @@ import (
 	"github.com/copernet/copernicus/model/outpoint"
 	"github.com/copernet/copernicus/model/script"
 	"github.com/copernet/copernicus/util"
+	"math"
+
+	//"github.com/magiconair/properties/assert"
+	"errors"
+	"github.com/stretchr/testify/assert"
 	"os"
 	"testing"
 )
+
+type TestWriter struct {
+	Rets [3]bool
+	Cnt  int
+}
+
+func (tw *TestWriter) Write(p []byte) (n int, err error) {
+	ret := tw.Rets[tw.Cnt]
+	tw.Cnt += 1
+	if ret {
+		return 1, nil
+	}
+	return 0, errors.New("test writer error")
+}
+
+type TestReader struct {
+	Cnt int
+	Idx int
+}
+
+func (tr *TestReader) Read(p []byte) (n int, err error) {
+	if tr.Cnt == tr.Idx {
+		return 0, errors.New("test reader error")
+	}
+	tr.Cnt += 1
+	return 1, nil
+}
 
 //var testTxIn *TxIn
 
@@ -93,4 +125,97 @@ func TestTxInSerialize(t *testing.T) {
 		t.Error(err)
 	}
 
+}
+
+func TestTxIn_SerializeSize(t *testing.T) {
+	sz := testTxIn.SerializeSize()
+	assert.Equal(t, sz, uint32(64))
+}
+
+func TestTxIn_Encode_PreviousOutputPoint_false(t *testing.T) {
+
+	w := TestWriter{Rets: [3]bool{false, false, false}, Cnt: 0}
+	assert.NotNil(t, testTxIn.Encode(&w))
+}
+
+func TestTxIn_Encode_scriptSig_false(t *testing.T) {
+	w := TestWriter{Rets: [3]bool{true, true, false}, Cnt: 0}
+	assert.NotNil(t, testTxIn.Encode(&w))
+}
+
+func TestTxIn_Decode_PreviousOutputPoint_false(t *testing.T) {
+	r := TestReader{Cnt: 0, Idx: 0}
+	assert.NotNil(t, testTxIn.Decode(&r))
+}
+
+func TestTxIn_Decode_Codebase_true(t *testing.T) {
+	file, err := os.OpenFile("tmp2.txt", os.O_RDWR|os.O_CREATE, 0666)
+	if err != nil {
+		t.Error(err)
+	}
+	defer func() {
+		err := os.Remove("tmp2.txt")
+		if err != nil {
+			t.Error(err)
+		}
+	}()
+
+	txin := testTxIn
+	txin.PreviousOutPoint.Index = 0xffffffff
+	err = txin.Serialize(file)
+	if err != nil {
+		t.Error(err)
+	}
+
+	_, err = file.Seek(0, 0)
+	if err != nil {
+		t.Error(err)
+	}
+
+	txInRead := &TxIn{}
+	txInRead.PreviousOutPoint = &outpoint.OutPoint{}
+	txInRead.PreviousOutPoint.Hash = util.Hash{}
+	txInRead.scriptSig = script.NewEmptyScript()
+
+	err = txInRead.Unserialize(file)
+	assert.NoError(t, err)
+}
+
+func TestTxIn_Decode_scriptSig_false(t *testing.T) {
+	r := TestReader{Cnt: 0, Idx: 63}
+	err := testTxIn.Decode(&r)
+	assert.NotNil(t, err)
+}
+
+func TestTxIn_GetScriptSig(t *testing.T) {
+	assert.Equal(t, sigScript, testTxIn.GetScriptSig())
+}
+
+func TestTxIn_SetScriptSig(t *testing.T) {
+	txin := &TxIn{}
+	txin.SetScriptSig(sigScript)
+	assert.Equal(t, sigScript, txin.scriptSig)
+}
+
+func TestTxIn_CheckStandard(t *testing.T) {
+	b, _ := testTxIn.CheckStandard()
+	assert.True(t, b)
+}
+
+func TestTxIn_String(t *testing.T) {
+	assert.Equal(t,
+		"PreviousOutPoint: OutPoint (hash:becc07bb21504e34fa035e7c959dda6ed1accf0e73fdb2a357b1bc31007e60c1 index: 4294967295)  , script:160014c3e2279d2ac730bd33c461744d8ed8e811f805db , Sequence:4294967295 ",
+		testTxIn.String())
+
+	txin := testTxIn
+	txin.SetScriptSig(nil)
+
+	assert.Equal(t,
+		"PreviousOutPoint: OutPoint (hash:becc07bb21504e34fa035e7c959dda6ed1accf0e73fdb2a357b1bc31007e60c1 index: 4294967295)  , script:  , Sequence:4294967295 ",
+		txin.String())
+}
+
+func TestNewTxIn2(t *testing.T) {
+	var txin = NewTxIn(nil, sigScript, sequence)
+	assert.Equal(t, outpoint.NewOutPoint(util.Hash{}, math.MaxUint32), txin.PreviousOutPoint)
 }
