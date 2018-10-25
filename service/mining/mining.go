68f82acf7380c9aa045e485a584c1dfe1be4b8b5
xiaolong.ran <rxl5555555@qq.com>
2018-10-25 14:05:28 +0800

    add err log for locator bug

--- service/mining/mining.go
+++ service/mining/mining.go
@@ -338,7 +338,7 @@ func (ba *BlockAssembler) CreateNewBlock(scriptPubKey, scriptSig *script.Script)
 
 	//check the validity of the block
 	if !TestBlockValidity(ba.bt.Block, indexPrev) {
-		log.Error("CreateNewBlock: TestBlockValidity failed.")
+		log.Error("CreateNewBlock: TestBlockValidity failed, block is:%v, indexPrev:%v", ba.bt.Block, indexPrev)
 		return nil
 	}
 
@@ -506,12 +506,12 @@ func TestBlockValidity(block *block.Block, indexPrev *blockindex.BlockIndex) boo
 	defer persist.CsMain.Unlock()
 
 	if !(indexPrev != nil && indexPrev == chain.GetInstance().Tip()) {
-		log.Error("TestBlockValidity(): indexPrev:%  chain tip:%v", indexPrev, chain.GetInstance().Tip())
+		log.Error("TestBlockValidity(): indexPrev:%v, chain tip:%v.", indexPrev, chain.GetInstance().Tip())
 		return false
 	}
 
 	if !lblockindex.CheckIndexAgainstCheckpoint(indexPrev) {
-		log.Error("mining: CheckIndexAgainstCheckpoint() failed, please check.")
+		log.Error("TestBlockValidity(): check index against check point failed, indexPrev:%v.", indexPrev)
 		return false
 	}
 
@@ -524,22 +524,22 @@ func TestBlockValidity(block *block.Block, indexPrev *blockindex.BlockIndex) boo
 
 	// NOTE: CheckBlockHeader is called by CheckBlock
 	if !lblock.ContextualCheckBlockHeader(&blkHeader, indexPrev, util.GetAdjustedTime()) {
-		log.Error("TestBlockValidity(): Consensus::ContextualCheckBlockHeader failed, please check.")
+		log.Error("TestBlockValidity():ContextualCheckBlockHeader failed, blkHeader:%v, indexPrev:%v.", blkHeader, indexPrev)
 		return false
 	}
 
 	if err := lblock.CheckBlock(block, false, false); err != nil {
-		log.Error("TestBlockValidity(): Consensus::CheckBlock failed: %v", err)
+		log.Error("TestBlockValidity(): check block:%v error: %v,", block, err)
 		return false
 	}
 
 	if err := lblock.ContextualCheckBlock(block, indexPrev); err != nil {
-		log.Error("TestBlockValidity(): Consensus::ContextualCheckBlock failed: %v", err)
+		log.Error("TestBlockValidity(): contextual check block:%v, indexPrev:%v error: %v", block, indexPrev, err)
 		return false
 	}
 
 	if err := lchain.ConnectBlock(block, indexDummy, coinMap, true); err != nil {
-		log.Error("trying to connect to the block failed:%v", err)
+		log.Error("trying to connect to the block:%v, indexDummy:%v, coinMap:%v, error:%v", block, indexDummy, coinMap, err)
 		return false
 	}
 
