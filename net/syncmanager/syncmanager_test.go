58b92ce0e32df7bc34f10fd041474497d0ee4322
xiaolong.ran <rxl5555555@qq.com>
2018-10-27 14:44:23 +0800

    add some test case for syncmanager

--- net/syncmanager/syncmanager_test.go
+++ net/syncmanager/syncmanager_test.go
@@ -30,6 +30,9 @@ import (
 	"github.com/copernet/copernicus/persist/db"
 	"github.com/copernet/copernicus/util"
 	"github.com/magiconair/properties/assert"
+	"io"
+	"net"
+	"time"
 )
 
 type mockPeerNotifier struct{}
@@ -378,4 +381,272 @@ func TestSyncManager_limitMap(t *testing.T) {
 	assert.Equal(t, len(mp), 2)
 	sm.limitMap(mp, 2)
 	assert.Equal(t, len(mp), 1)
+}
+
+//func TestSyncManager_handleInvMsg(t *testing.T) {
+//	sm, dir, err := makeSyncManager()
+//	if err != nil {
+//		t.Fatalf("construct syncmanager failed :%v\n", err)
+//	}
+//	defer os.RemoveAll(dir)
+//
+//
+//	hash1 := util.HashFromString("00000000000001bcd6b635a1249dfbe76c0d001592a7219a36cd9bbd002c7238")
+//	hash2 := util.HashFromString("00000000000001bcd6b635a1249dfbe76c0d001592a7219a36cd9bbd002c7239")
+//	invVect1 := wire.NewInvVect(wire.InvTypeTx, hash1)
+//	//invVect2 := wire.NewInvVect(wire.InvTypeBlock, hash2)
+//
+//	msgInv := wire.NewMsgInv()
+//	msgInv.AddInvVect(invVect1)
+//	//msgInv.AddInvVect(invVect2)
+//
+//	p := &peer.Peer{}
+//
+//	//inv := &invMsg{
+//	//	inv:  msgInv,
+//	//	peer: p,
+//	//}
+//
+//	requestedTxns := make(map[util.Hash]struct{})
+//	requestedTxns[*hash1] = struct{}{}
+//	requestedTxns[*hash2] = struct{}{}
+//
+//	requestedBlocks := make(map[util.Hash]struct{})
+//	requestedBlocks[*hash1] = struct{}{}
+//	requestedBlocks[*hash2] = struct{}{}
+//
+//	syncState := &peerSyncState{
+//		syncCandidate:   false,
+//		requestQueue:    msgInv.InvList,
+//		requestedTxns:   requestedTxns,
+//		requestedBlocks: requestedBlocks,
+//	}
+//
+//	sm.peerStates[p] = syncState
+//	sm.Start()
+//
+//}
+
+func TestSyncManager_resetHeaderState(t *testing.T) {
+	sm, dir, err := makeSyncManager()
+	if err != nil {
+		t.Fatalf("construct syncmanager failed :%v\n", err)
+	}
+	defer os.RemoveAll(dir)
+	sm.Start()
+
+	hash1 := util.HashFromString("00000000000001bcd6b635a1249dfbe76c0d001592a7219a36cd9bbd002c7238")
+	checkPoint := &model.Checkpoint{
+		Height: 11,
+		Hash:   hash1,
+	}
+	sm.nextCheckpoint = checkPoint
+	sm.resetHeaderState(hash1, 10)
+	assert.Equal(t, sm.nextCheckpoint.Height, int32(11))
+	assert.Equal(t, sm.nextCheckpoint.Hash, hash1)
+	assert.Equal(t, sm.headersFirstMode, false)
+}
+
+func TestSyncManager_findNextHeaderCheckpoint(t *testing.T) {
+	sm, dir, err := makeSyncManager()
+	if err != nil {
+		t.Fatalf("construct syncmanager failed :%v\n", err)
+	}
+	defer os.RemoveAll(dir)
+	sm.Start()
+
+	hash1 := util.HashFromString("00000000000001bcd6b635a1249dfbe76c0d001592a7219a36cd9bbd002c7238")
+	hash2 := util.HashFromString("00000000000001bcd6b635a1249dfbe76c0d001592a7219a36cd9bbd002c7239")
+	checkPoint1 := &model.Checkpoint{
+		Height: 11,
+		Hash:   hash1,
+	}
+
+	checkPoint2 := &model.Checkpoint{
+		Height: 12,
+		Hash:   hash2,
+	}
+
+	cp := make([]*model.Checkpoint, 0, 10)
+	cp = append(cp, checkPoint1)
+	cp = append(cp, checkPoint2)
+	model.ActiveNetParams.Checkpoints = cp
+	ckPoint := sm.findNextHeaderCheckpoint(10)
+	assert.Equal(t, ckPoint, checkPoint1)
+
+	ckPoint = sm.findNextHeaderCheckpoint(11)
+	assert.Equal(t, ckPoint, checkPoint2)
+
+	ckPoint = sm.findNextHeaderCheckpoint(110)
+	if ckPoint != nil {
+		t.Errorf("checkPoint header too large, ckPoint:%v", ckPoint)
+	}
+
+	cp1 := make([]*model.Checkpoint, 0)
+	model.ActiveNetParams.Checkpoints = cp1
+	ckPoint1 := sm.findNextHeaderCheckpoint(11)
+	if ckPoint1 != nil {
+		t.Errorf("find next header checkPoint failed, ckPoint1:%v", ckPoint1)
+	}
+}
+
+type conn struct {
+	io.Reader
+	io.Writer
+	io.Closer
+
+	// local network, address for the connection.
+	lnet, laddr string
+	// remote network, address for the connection.
+	rnet, raddr string
+	// mocks socks proxy if true
+	proxy bool
+}
+
+// LocalAddr returns the local address for the connection.
+func (c conn) LocalAddr() net.Addr {
+	return &addr{c.lnet, c.laddr}
+}
+
+// Remote returns the remote address for the connection.
+func (c conn) RemoteAddr() net.Addr {
+	if !c.proxy {
+		return &addr{c.rnet, c.raddr}
+	}
+	panic("proxy mode is not supported yet")
+}
+
+// Close handles closing the connection.
+func (c conn) Close() error {
+	if c.Closer == nil {
+		return nil
+	}
+	return c.Closer.Close()
+}
+
+func (c conn) SetDeadline(t time.Time) error      { return nil }
+func (c conn) SetReadDeadline(t time.Time) error  { return nil }
+func (c conn) SetWriteDeadline(t time.Time) error { return nil }
+
+// addr mocks a network address
+type addr struct {
+	net, address string
+}
+
+func (m addr) Network() string { return m.net }
+func (m addr) String() string  { return m.address }
+
+// pipe turns two mock connections into a full-duplex connection similar to
+// net.Pipe to allow pipe's with (fake) addresses.
+func pipe(c1, c2 *conn) (*conn, *conn) {
+	r1, w1 := io.Pipe()
+	r2, w2 := io.Pipe()
+
+	c1.Writer = w1
+	c1.Closer = w1
+	c2.Reader = r1
+	c1.Reader = r2
+	c2.Writer = w2
+	c2.Closer = w2
+
+	return c1, c2
+}
+
+func TestSyncManager_isSyncCandidate(t *testing.T) {
+	sm, dir, err := makeSyncManager()
+	if err != nil {
+		t.Fatalf("construct syncmanager failed :%v\n", err)
+	}
+	defer os.RemoveAll(dir)
+	sm.chainParams = &model.TestNetParams
+	sm.Start()
+
+	verack := make(chan struct{})
+	peer1Cfg := &peer.Config{
+		Listeners: peer.MessageListeners{
+			OnVerAck: func(p *peer.Peer, msg *wire.MsgVerAck) {
+				verack <- struct{}{}
+			},
+			OnWrite: func(p *peer.Peer, bytesWritten int, msg wire.Message,
+				err error) {
+				if _, ok := msg.(*wire.MsgVerAck); ok {
+					verack <- struct{}{}
+				}
+			},
+		},
+		UserAgentName:     "peer",
+		UserAgentVersion:  "1.0",
+		UserAgentComments: []string{"comment"},
+		ChainParams:       &model.MainNetParams,
+		ProtocolVersion:   wire.RejectVersion, // Configure with older version
+		Services:          0,
+	}
+
+	tests := []struct {
+		name  string
+		setup func() (*peer.Peer, *peer.Peer, error)
+	}{
+		{
+			name: "basic handshake",
+			setup: func() (*peer.Peer, *peer.Peer, error) {
+				pipe(
+					&conn{raddr: "10.0.0.1:8333", laddr: "10.0.0.1:18333"},
+					&conn{raddr: "10.0.0.2:8333", laddr: "10.0.0.2:18333"},
+				)
+				inPeer := peer.NewInboundPeer(peer1Cfg)
+
+				return inPeer, nil, nil
+			},
+		},
+	}
+
+	for i, test := range tests {
+		inPeer, _, err := test.setup()
+		if err != nil {
+			t.Errorf("TestPeerConnection setup #%d: unexpected err %v", i, err)
+			return
+		}
+		sm.isSyncCandidate(inPeer)
+
+		hash1 := util.HashFromString("00000000000001bcd6b635a1249dfbe76c0d001592a7219a36cd9bbd002c7238")
+		invVect1 := wire.NewInvVect(wire.InvTypeTx, hash1)
+		msgInv := wire.NewMsgInv()
+		msgInv.AddInvVect(invVect1)
+
+		requestedTxns := make(map[util.Hash]struct{})
+		requestedTxns[*hash1] = struct{}{}
+
+		requestedBlocks := make(map[util.Hash]struct{})
+		requestedBlocks[*hash1] = struct{}{}
+
+		//test one case
+		syncState := &peerSyncState{
+			syncCandidate:   true,
+			requestQueue:    msgInv.InvList,
+			requestedTxns:   requestedTxns,
+			requestedBlocks: requestedBlocks,
+		}
+		sm.peerStates[inPeer] = syncState
+		chain.GetInstance().Tip().Height = 10
+		sm.startSync()
+
+		//test two case
+		checkPoint1 := &model.Checkpoint{
+			Height: 11,
+			Hash:   hash1,
+		}
+		syncState.syncCandidate = true
+		sm.nextCheckpoint = checkPoint1
+		sm.startSync()
+
+		sm.chainParams = &model.RegressionNetParams
+		sm.startSync()
+		//test three case
+		syncState.syncCandidate = true
+		chain.GetInstance().Tip().Height = 0
+		sm.startSync()
+
+		//close connection
+		inPeer.Disconnect()
+	}
 }
