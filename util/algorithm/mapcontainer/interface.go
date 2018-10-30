72a8d6c0b01d2f5c8959958c69cc2eec7e858fed
qiwei9743 <qiwei9743@gmail.com>
2018-10-30 10:46:39 +0800

    refine interface for mapcontainer.

--- util/algorithm/mapcontainer/interface.go
+++ util/algorithm/mapcontainer/interface.go
@@ -5,14 +5,12 @@ type Lesser interface {
 }
 
 type MapContainer interface {
-	//Insert(Lesser) bool
 	Search(Lesser) (Lesser, bool)
 	ReplaceOrInsert(Lesser) Lesser
 	Delete(Lesser) (deleted Lesser, found bool)
 	Min() (less Lesser, found bool)
-	Ascend(iterHandler func(i Lesser) bool)
-
-	Len() int
 	Max() (less Lesser, found bool)
+	Ascend(iterHandler func(i Lesser) bool)
+	Len() int
 	DeleteMax() (less Lesser, found bool)
 }
