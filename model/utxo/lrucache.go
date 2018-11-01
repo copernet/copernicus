abb7b8173bb2d9d64b68f02bcf8d507a80f6aae9
xiaolong.ran <rxl5555555@qq.com>
2018-11-01 18:28:33 +0800

    Revert "Revert "add LimitMempoolSize logic for mempool""
    This reverts commit 5a34cbae431e1d59aeb2c5a4037b7efe002eb3b9.

--- model/utxo/lrucache.go
+++ model/utxo/lrucache.go
@@ -3,6 +3,7 @@ package utxo
 import (
 	"unsafe"
 
+	"github.com/copernet/copernicus/errcode"
 	"github.com/copernet/copernicus/log"
 	"github.com/copernet/copernicus/model/outpoint"
 	"github.com/copernet/copernicus/util"
@@ -70,6 +71,16 @@ func (coinsCache *CoinsLruCache) GetCoin(outpoint *outpoint.OutPoint) *Coin {
 func (coinsCache *CoinsLruCache) HaveCoin(point *outpoint.OutPoint) bool {
 	coin := coinsCache.GetCoin(point)
 	return coin != nil && !coin.IsSpent()
+}
+
+func (coinsCache *CoinsLruCache) RemoveCoins(point *outpoint.OutPoint) error {
+	coinsCache.cacheCoins.Remove(*point)
+	coin := coinsCache.GetCoin(point)
+	if coin != nil {
+		log.Error("the coin already remove coin is:%v", coin)
+		return errcode.NewError(errcode.RejectInvalid, "the coin already remove")
+	}
+	return nil
 }
 
 func (coinsCache *CoinsLruCache) GetBestBlock() (util.Hash, error) {
