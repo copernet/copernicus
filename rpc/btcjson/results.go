53860bdf28c50f7075e5f6c31f671f4c6e849cd9
georgefff <514625951@qq.com>
2018-11-03 08:16:25 +0800

    add rpc gettransactionResult

--- rpc/btcjson/results.go
+++ rpc/btcjson/results.go
@@ -643,13 +643,11 @@ type GetTransactionDetailsResult struct {
 type GetTransactionResult struct {
 	Amount          float64                       `json:"amount"`
 	Fee             float64                       `json:"fee,omitempty"`
-	Confirmations   int64                         `json:"confirmations"`
+	Confirmations   int32                         `json:"confirmations"`
 	BlockHash       string                        `json:"blockhash"`
-	BlockIndex      int64                         `json:"blockindex"`
-	BlockTime       int64                         `json:"blocktime"`
+	BlockTime       uint32                        `json:"blocktime"`
 	TxID            string                        `json:"txid"`
 	WalletConflicts []string                      `json:"walletconflicts"`
-	Time            int64                         `json:"time"`
 	TimeReceived    int64                         `json:"timereceived"`
 	Details         []GetTransactionDetailsResult `json:"details"`
 	Hex             string                        `json:"hex"`
--- rpc/rpcwallet.go
+++ rpc/rpcwallet.go
@@ -10,6 +10,7 @@ import (
 	"github.com/copernet/copernicus/model/tx"
 	"github.com/copernet/copernicus/model/wallet"
 	"github.com/copernet/copernicus/rpc/btcjson"
+	"github.com/copernet/copernicus/util"
 	"github.com/copernet/copernicus/util/amount"
 	"github.com/pkg/errors"
 )
@@ -191,7 +192,45 @@ func handleGetTransaction(s *Server, cmd interface{}, closeChan <-chan struct{})
 	if !lwallet.IsWalletEnable() {
 		return nil, walletDisableRPCError
 	}
-	return nil, nil
+	c := cmd.(*btcjson.GetTransactionCmd)
+	pwallet := wallet.GetInstance()
+	txHash, err := util.GetHashFromStr(c.Txid)
+	if err != nil {
+		return nil, errors.New("Tx Hash is err")
+	}
+	wtx := pwallet.GetWalletTx(*txHash)
+	if wtx == nil {
+		return nil, errors.New("Invalid or non-wallet transaction id")
+	}
+
+	ret := btcjson.GetTransactionResult{}
+	filter := wallet.ISMINE_SPENDABLE
+	credit := wtx.GetCredit(filter)
+	debit := wtx.GetDebit(filter)
+	net := credit - debit
+	var fee amount.Amount
+	if debit > 0 {
+		fee = wtx.Tx.GetValueOut() - debit
+	}
+
+	// Fill GetTransactionResult
+	ret.Amount = (net - fee).ToBTC()
+	if debit > 0 {
+		ret.Fee = fee.ToBTC()
+	}
+	ret.Confirmations = wtx.GetDepthInMainChain()
+	if ret.Confirmations > 0 {
+		index := chain.GetInstance().GetIndex(wtx.GetBlokHeight())
+		ret.BlockHash = index.GetBlockHash().String()
+		ret.BlockTime = index.GetBlockTime()
+	}
+	ret.TxID = c.Txid
+	ret.WalletConflicts = nil
+	ret.TimeReceived = wtx.TimeReceived
+
+	// Fill GetTransactionDetailsResult
+
+	return ret, nil
 }
 
 func sendMoney(scriptPubKey *script.Script, value amount.Amount, subtractFeeFromAmount bool,
