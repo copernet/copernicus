d52568fc395ecd7913bef6ca688225e72e9045ed
wangshouyan <wangshouyan@gmail.com>
2018-11-13 21:27:31 +0800

    add support of download blockdata parallel with several peer, each peer at most 16 getdata request

--- model/chain/chain.go
+++ model/chain/chain.go
@@ -599,3 +599,7 @@ func (c *Chain) BuildForwardTree() (forward map[*blockindex.BlockIndex][]*blocki
 	}
 	return
 }
+
+func (c *Chain) CanDirectFetch() bool {
+	return int64(c.Tip().GetBlockTime()) > util.GetAdjustedTime() * int64(c.params.TargetTimePerBlock)
+}
