7ee65f4c3ca46de40a8e02a511e588bf912b58a3
wangshouyan <wangshouyan@gmail.com>
2018-11-14 11:17:49 +0800

    bug fix: CanDirectFetch ref abc

--- model/chain/chain.go
+++ model/chain/chain.go
@@ -601,5 +601,5 @@ func (c *Chain) BuildForwardTree() (forward map[*blockindex.BlockIndex][]*blocki
 }
 
 func (c *Chain) CanDirectFetch() bool {
-	return int64(c.Tip().GetBlockTime()) > util.GetAdjustedTime() * int64(c.params.TargetTimePerBlock)
+	return int64(c.Tip().GetBlockTime()) > util.GetAdjustedTime() - int64(c.params.TargetTimePerBlock) * 20
 }
