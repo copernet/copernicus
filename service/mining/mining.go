daad6eb68a0ba2a84e349d12f043639f31d32e03
冉小龙 <rxl5555555@qq.com>
2018-11-16 18:16:49 +0800

    fix a little

--- service/mining/mining.go
+++ service/mining/mining.go
@@ -342,12 +342,12 @@ func (ba *BlockAssembler) CreateNewBlock(scriptPubKey, scriptSig *script.Script)
 	if model.IsMagneticAnomalyEnabled(indexPrev.GetMedianTimePast()) {
 		// If magnetic anomaly is enabled, we make sure transaction are
 		// canonically ordered.
-		tmpTxIDs := make([]*util.Hash, len(ba.bt.Block.Txs)-1)
+		tmpTxIDs := make([]*util.Hash, len(ba.bt.Block.Txs))
 		for _, tmpTx := range ba.bt.Block.Txs {
 			hash := tmpTx.GetHash()
 			tmpTxIDs = append(tmpTxIDs, &hash)
 		}
-		tmpSortTxIDs := sortTxs(tmpTxIDs[1:])
+		tmpSortTxIDs := sortTxs(tmpTxIDs)
 		sort.Sort(tmpSortTxIDs)
 
 		for _, sortHash := range tmpTxIDs {
