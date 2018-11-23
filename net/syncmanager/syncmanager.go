5b4ba42b8410a809c52f7614cd6945870741e4d8
wangshouyan <wangshouyan@gmail.com>
2018-11-23 11:22:04 +0800

    fix situation of syncPeer not at top during IBD

--- net/syncmanager/syncmanager.go
+++ net/syncmanager/syncmanager.go
@@ -692,16 +692,7 @@ func (sm *SyncManager) syncPoints(peer *peer.Peer) (pindexWalk, pindexBestKnownB
 	isIBDSyncing := sm.syncPeer != nil && lchain.IsInitialBlockDownload()
 
 	if isIBDSyncing {
-		idx := lastAccouncedBlock(sm.syncPeer)
-		if idx == nil {
-			pindexStart := gChain.GetIndexBestHeader()
-			if pindexStart.Prev != nil {
-				pindexStart = pindexStart.Prev
-			}
-			locator := gChain.GetLocator(pindexStart)
-			sm.syncPeer.PushGetHeadersMsg(*locator, &zeroHash)
-		}
-		return gChain.Tip(), idx
+		return gChain.Tip(), gChain.GetIndexBestHeader()
 	}
 
 	if pindexBestKnownBlock = lastAccouncedBlock(peer); pindexBestKnownBlock != nil {
