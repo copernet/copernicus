132eda07c11efe36c0921074e40785a80f5e96c2
zymap <734206637@qq.com>
2018-11-24 23:22:00 +0800

    When execute dockerpush, should exit 0 and alert ci

--- dockerpush.sh
+++ dockerpush.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 jobID=$1
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-containerID=`docker ps | awk '{if(NR==2){print $1}}'`
-echo 'docker commit' $containerID 'copernet/copernicus:'$jobID
-docker commit $containerID copernet/copernicus:$jobID
-docker push copernet/copernicus:$jobID
+#echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+#containerID=`docker ps | awk '{if(NR==2){print $1}}'`
+#echo 'docker commit' $containerID 'copernet/copernicus:'$jobID
+#docker commit $containerID copernet/copernicus:$jobID
+#docker push copernet/copernicus:$jobID
 echo 'docker push done. tag:' $jobID
+exit 1
